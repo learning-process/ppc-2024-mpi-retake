1cdb74c2a9508798ce53d53dacdfd701a067f5ae
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2025-02-12 08:33:02 +0300

    Update ops_mpi.cpp

--- tasks/mpi/deryabin_m_cannons_algorithm/src/ops_mpi.cpp
+++ tasks/mpi/deryabin_m_cannons_algorithm/src/ops_mpi.cpp
@@ -101,13 +101,13 @@ bool deryabin_m_cannons_algorithm_mpi::CannonsAlgorithmMPITaskParallel::RunImpl(
       block_rows_columns = (unsigned short)sqrt(world_.size());
       block_dimension = dimension / block_rows_columns;
     }
-    boost::mpi::broadcast(world_, dimension, 0);
+    broadcast(world_, dimension, 0);
     output_matrix_C_ = std::vector<double>(dimension * dimension);
-    boost::mpi::broadcast(world_, block_dimension, 0);
+    broadcast(world_, block_dimension, 0);
     local_input_matrix_A_ = std::vector<double>(block_dimension * block_dimension);
     local_input_matrix_B_ = std::vector<double>(block_dimension * block_dimension);
     local_output_matrix_C_ = std::vector<double>(block_dimension * block_dimension);
-    boost::mpi::broadcast(world_, block_rows_columns, 0);
+    broadcast(world_, block_rows_columns, 0);
     if (world_.rank() == 0) {
       k = 0;
       while (k != block_dimension) {
