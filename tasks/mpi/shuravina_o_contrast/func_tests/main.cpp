762c3ddf16ecbe07b68960a09de4f37b65cbda03
shyravina19102004 <shyravina19102004@gmail.com>
2025-02-13 21:22:45 +0300

    2

--- tasks/mpi/shuravina_o_contrast/func_tests/main.cpp
+++ tasks/mpi/shuravina_o_contrast/func_tests/main.cpp
@@ -37,6 +37,7 @@ TEST(shuravina_o_contrast, test_contrast_increase) {
 
   EXPECT_NE(in, out);
 }
+
 TEST(shuravina_o_contrast, test_min_max_values) {
   constexpr size_t kCount = 256;
 
@@ -66,6 +67,7 @@ TEST(shuravina_o_contrast, test_min_max_values) {
   EXPECT_EQ(min_val, 0);
   EXPECT_EQ(max_val, 255);
 }
+
 TEST(shuravina_o_contrast, test_all_values_equal) {
   constexpr size_t kCount = 256;
 
@@ -88,54 +90,4 @@ TEST(shuravina_o_contrast, test_all_values_equal) {
   for (size_t i = 1; i < out.size(); ++i) {
     EXPECT_EQ(out[i], first_val);
   }
-}
-TEST(shuravina_o_contrast, test_small_image) {
-  constexpr size_t kCount = 2;
-
-  std::vector<uint8_t> in = {10, 20, 30, 40};
-  std::vector<uint8_t> out(kCount * kCount, 0);
-
-  auto task_data_mpi = std::make_shared<ppc::core::TaskData>();
-  task_data_mpi->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
-  task_data_mpi->inputs_count.emplace_back(in.size());
-  task_data_mpi->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
-  task_data_mpi->outputs_count.emplace_back(out.size());
-
-  shuravina_o_contrast::TestTaskMPI test_task_mpi(task_data_mpi);
-  ASSERT_EQ(test_task_mpi.Validation(), true);
-  test_task_mpi.PreProcessing();
-  test_task_mpi.Run();
-  test_task_mpi.PostProcessing();
-
-  EXPECT_NE(in, out);
-  EXPECT_EQ(out[0], 0);
-  EXPECT_EQ(out[1], 85);
-  EXPECT_EQ(out[2], 170);
-  EXPECT_EQ(out[3], 255);
-}
-
-TEST(shuravina_o_contrast, test_random_values) {
-  constexpr size_t kCount = 256;
-
-  std::vector<uint8_t> in(kCount * kCount);
-  std::vector<uint8_t> out(kCount * kCount, 0);
-
-  std::srand(static_cast<unsigned int>(std::time(nullptr)));
-  for (size_t i = 0; i < in.size(); ++i) {
-    in[i] = static_cast<uint8_t>(std::rand() % 256);
-  }
-
-  auto task_data_mpi = std::make_shared<ppc::core::TaskData>();
-  task_data_mpi->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
-  task_data_mpi->inputs_count.emplace_back(in.size());
-  task_data_mpi->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
-  task_data_mpi->outputs_count.emplace_back(out.size());
-
-  shuravina_o_contrast::TestTaskMPI test_task_mpi(task_data_mpi);
-  ASSERT_EQ(test_task_mpi.Validation(), true);
-  test_task_mpi.PreProcessing();
-  test_task_mpi.Run();
-  test_task_mpi.PostProcessing();
-
-  EXPECT_NE(in, out);
 }
