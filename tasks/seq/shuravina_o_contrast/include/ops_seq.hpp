9ad1d6f40ad0661a9bc85122d551c3f98069e979
shyravina19102004 <shyravina19102004@gmail.com>
2025-02-13 23:15:57 +0300

    8

--- tasks/seq/shuravina_o_contrast/include/ops_seq.hpp
+++ tasks/seq/shuravina_o_contrast/include/ops_seq.hpp
@@ -10,14 +10,18 @@ namespace shuravina_o_contrast {
 class ContrastTaskSequential : public ppc::core::Task {
  public:
   explicit ContrastTaskSequential(ppc::core::TaskDataPtr task_data) : Task(std::move(task_data)) {}
+
   bool PreProcessingImpl() override;
   bool ValidationImpl() override;
   bool RunImpl() override;
   bool PostProcessingImpl() override;
 
  private:
-  std::vector<uint8_t> input_, output_;
+  std::vector<uint8_t> input_;
+  std::vector<uint8_t> output_;
   int width_{}, height_{};
+
+  void IncreaseContrast();
 };
 
 }  // namespace shuravina_o_contrast
