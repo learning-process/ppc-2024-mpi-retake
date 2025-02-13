2f6aa852d1a25cd78d465ff6605536ed820df81a
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2025-02-13 03:01:29 +0300

    Update ops_seq.cpp

--- tasks/seq/deryabin_m_cannons_algorithm/src/ops_seq.cpp
+++ tasks/seq/deryabin_m_cannons_algorithm/src/ops_seq.cpp
@@ -19,7 +19,7 @@ bool deryabin_m_cannons_algorithm_seq::CannonsAlgorithmTaskSequential::RunImpl()
   unsigned short i = 0;
   unsigned short j = 0;
   unsigned short count = 0;
-  auto dimension = (unsigned short)sqrt(input_matrix_A_.size());
+  auto dimension = (unsigned short)sqrt(static_cast<unsigned short>(input_matrix_A_.size()));
   while (i != dimension) {
     j = 0;
     while (j != dimension) {
