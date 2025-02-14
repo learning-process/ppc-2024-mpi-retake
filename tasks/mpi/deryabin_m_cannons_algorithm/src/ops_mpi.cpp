ca0d08672a3feb6d5e8a81ad990bc987a5749dff
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2025-02-14 17:37:40 +0300

    Update ops_mpi.cpp

--- tasks/mpi/deryabin_m_cannons_algorithm/src/ops_mpi.cpp
+++ tasks/mpi/deryabin_m_cannons_algorithm/src/ops_mpi.cpp
@@ -70,10 +70,10 @@ bool deryabin_m_cannons_algorithm_mpi::CannonsAlgorithmMPITaskParallel::Validati
   return true;
 }
 
-[[nodiscard]] bool deryabin_m_cannons_algorithm_mpi::CannonsAlgorithmMPITaskParallel::IsTrivialCase() const {
-  return (world.size() == 1 || world.size() != pow((unsigned short)sqrt(world.size()), 2) ||
-      (unsigned short)sqrt(input_matrix_A.size()) % (unsigned short)sqrt(world.size()) != 0);
-}
+ [[nodiscard]] bool deryabin_m_cannons_algorithm_mpi::CannonsAlgorithmMPITaskParallel::IsTrivialCase() const {
+   return (world.size() == 1 || world.size() != pow((unsigned short)sqrt(world.size()), 2) ||
+          (unsigned short)sqrt(input_matrix_A.size()) % (unsigned short)sqrt(world.size()) != 0);
+ }
 
 void deryabin_m_cannons_algorithm_mpi::CannonsAlgorithmMPITaskParallel::HandleTrivialCase() {
   if (world_.rank() == 0) {
