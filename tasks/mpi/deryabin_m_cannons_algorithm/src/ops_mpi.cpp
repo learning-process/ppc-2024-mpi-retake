0b03fd2c585270419be7a16f35c290b3e13cc367
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2025-02-15 20:40:27 +0300

    Update ops_mpi.cpp

--- tasks/mpi/deryabin_m_cannons_algorithm/src/ops_mpi.cpp
+++ tasks/mpi/deryabin_m_cannons_algorithm/src/ops_mpi.cpp
@@ -71,8 +71,8 @@ bool deryabin_m_cannons_algorithm_mpi::CannonsAlgorithmMPITaskParallel::Validati
 }
 
 [[nodiscard]] bool deryabin_m_cannons_algorithm_mpi::CannonsAlgorithmMPITaskParallel::IsTrivialCase() const {
-  return ((unsigned short)sqrt(static_cast<unsigned short>(input_matrix_A_.size())) <
-          (unsigned short)sqrt(static_cast<unsigned short>(world_.size())));
+  return (static_cast<unsigned short>(input_matrix_A_.size()) <
+          static_cast<unsigned short>(world_.size()));
 }
 
 void deryabin_m_cannons_algorithm_mpi::CannonsAlgorithmMPITaskParallel::HandleTrivialCase() {
