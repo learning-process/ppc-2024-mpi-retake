498fe99b3b4c5d19bfef9b3a9f5c1df9746253c1
Kate <ka10nnn@yandex.ru>
2025-02-15 08:01:26 +0300

    ub ggc build

--- tasks/mpi/budazhapova_e_count_freq_character/src/count_freq_of_ch_mpi.cpp
+++ tasks/mpi/budazhapova_e_count_freq_character/src/count_freq_of_ch_mpi.cpp
@@ -3,7 +3,7 @@
 
 #include "mpi/budazhapova_e_count_freq_character/include/count_freq_chart_mpi_header.hpp"
 
-int budazhapova_e_count_freq_counter_mpi::counting_freq(std::string str, char symb) {
+int budazhapova_e_count_freq_chart_mpi::counting_freq(std::string str, char symb) {
   int resalt = 0;
   for (unsigned long i = 0; i < str.length(); i++) {
     if (str[i] == symb) {
@@ -28,7 +28,7 @@ bool budazhapova_e_count_freq_chart_mpi::TestMPITaskSequential::ValidationImpl()
 
 bool budazhapova_e_count_freq_chart_mpi::TestMPITaskSequential::RunImpl() {
   InternalOrderTest();
-  res_ = counting_freq(input_, symb);
+  res_ = counting_freq(input_, symb_);
   return true;
 }
 
@@ -40,7 +40,7 @@ bool budazhapova_e_count_freq_chart_mpi::TestMPITaskSequential::PostProcessingIm
 
 bool budazhapova_e_count_freq_chart_mpi::TestMPITaskParallel::PreProcessingImpl() {
   InternalOrderTest();
-  int world_rank = world.rank();
+  int world_rank = world_.rank();
 
   if (world_rank == 0) {
     input_ = std::string(reinterpret_cast<char*>(task_data->inputs[0]), static_cast<int>(task_data->inputs_count[0]));
@@ -51,7 +51,7 @@ bool budazhapova_e_count_freq_chart_mpi::TestMPITaskParallel::PreProcessingImpl(
 
 bool budazhapova_e_count_freq_chart_mpi::TestMPITaskParallel::ValidationImpl() {
   InternalOrderTest();
-  if (world.rank() == 0) {
+  if (world_.rank() == 0) {
     return task_data->outputs_count[0] == 1;
   }
   return true;
@@ -59,15 +59,15 @@ bool budazhapova_e_count_freq_chart_mpi::TestMPITaskParallel::ValidationImpl() {
 
 bool budazhapova_e_count_freq_chart_mpi::TestMPITaskParallel::RunImpl() {
   InternalOrderTest();
-  int world_rank = world.rank();
+  int world_rank = world_.rank();
   int delta = 0;
   if (world_rank == 0) {
     int input_size = static_cast<int>(task_data->inputs_count[0]);
-    delta = (input_size % world.size() == 0) ? (input_size / world.size()) : ((input_size / world.size()) + 1);
+    delta = (input_size % world_.size() == 0) ? (input_size / world_.size()) : ((input_size / world_.size()) + 1);
   }
 
   boost::mpi::broadcast(world, delta, 0);
-  boost::mpi::broadcast(world, symb, 0);
+  boost::mpi::broadcast(world, symb_, 0);
 
   if (world_rank == 0) {
     for (int proc = 1; proc < world.size(); proc++) {
@@ -80,14 +80,14 @@ bool budazhapova_e_count_freq_chart_mpi::TestMPITaskParallel::RunImpl() {
   } else {
     world.recv(0, 0, local_input_.data(), delta);
   }
-  local_res_ = counting_freq(local_input_, symb);
+  local_res_ = counting_freq(local_input_, symb_);
   boost::mpi::reduce(world, local_res_, res_, std::plus<>(), 0);
   return true;
 }
 
 bool budazhapova_e_count_freq_chart_mpi::TestMPITaskParallel::PostProcessingImpl() {
   InternalOrderTest();
-  if (world.rank() == 0) {
+  if (world_.rank() == 0) {
     reinterpret_cast<int*>(task_data->outputs[0])[0] = res_;
   }
   return true;
