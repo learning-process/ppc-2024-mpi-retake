758897ed65c9224c37a7e68dbddea6d64565c9dc
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2025-02-16 16:25:25 +0300

    Update ops_mpi.cpp

--- tasks/mpi/deryabin_m_cannons_algorithm/src/ops_mpi.cpp
+++ tasks/mpi/deryabin_m_cannons_algorithm/src/ops_mpi.cpp
@@ -81,9 +81,9 @@ void deryabin_m_cannons_algorithm_mpi::CannonsAlgorithmMPITaskParallel::HandleTr
   if (world_.rank() == 0) {
     dimension_ = static_cast<unsigned short>(std::sqrt(static_cast<unsigned short>(input_matrix_A_.size())));
     output_matrix_C_.resize(dimension_ * dimension_, 0.0);
-    for (unsigned short i = 0; i < dimension_; ++i) {
-      for (unsigned short j = 0; j < dimension_; ++j) {
-        for (unsigned short k = 0; k < dimension_; ++k) {
+    for (unsigned short i = 0; i < dimension_; i++) {
+      for (unsigned short j = 0; j < dimension_; j++) {
+        for (unsigned short k = 0; k < dimension_; k++) {
           output_matrix_C_[(i * dimension_) + j] +=
               input_matrix_A_[(i * dimension_) + k] * input_matrix_B_[(k * dimension_) + j];
         }
