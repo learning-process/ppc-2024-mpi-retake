c8c540ecc453cb35baf2c4a2b42326573084ec0e
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2025-02-16 18:21:33 +0300

    Update ops_mpi.hpp

--- tasks/mpi/deryabin_m_cannons_algorithm/include/ops_mpi.hpp
+++ tasks/mpi/deryabin_m_cannons_algorithm/include/ops_mpi.hpp
@@ -33,7 +33,7 @@ class CannonsAlgorithmMPITaskParallel : public ppc::core::Task {
 
  private:
   // --- Приватные методы для декомпозиции ---
-  bool IsNotTrivialCase() const;
+  [[nodiscard]] bool IsNotTrivialCase() const;
   void HandleTrivialCase();
   void PerformCannonAlgorithm();
 
