a60d1c926984acb55c9c1904869c045930b100f4
Katerina Karaseva <k.karaseva2010@mail.ru>
2025-02-17 07:38:44 +0300

    error correction

--- tasks/mpi/karaseva_e_reduce/src/ops_mpi.cpp
+++ tasks/mpi/karaseva_e_reduce/src/ops_mpi.cpp
@@ -1,26 +1,34 @@
 #include "mpi/karaseva_e_reduce/include/ops_mpi.hpp"
 
+#include <algorithm>
 #include <boost/mpi/collectives.hpp>
-#include <cmath>
-#include <cstddef>
+#include <boost/mpi/communicator.hpp>
+#include <functional>
 #include <iostream>
-#include <numeric>
+#include <ranges>
 #include <vector>
 
 bool karaseva_e_reduce_mpi::TestTaskMPI::PreProcessingImpl() {
+  if (!task_data || task_data->inputs.empty() || task_data->outputs.empty()) {
+    return false;
+  }
+
   unsigned int input_size = task_data->inputs_count[0];
   auto *in_ptr = reinterpret_cast<int *>(task_data->inputs[0]);
-  input_.assign(in_ptr, in_ptr + input_size);
+  if (!in_ptr) {
+    return false;
+  }
 
+  input_.assign(in_ptr, in_ptr + input_size);
   unsigned int output_size = task_data->outputs_count[0];
   output_.assign(output_size, 0);
 
-  size_ = input_size;
+  size_ = static_cast<int>(input_size);
   return true;
 }
 
 bool karaseva_e_reduce_mpi::TestTaskMPI::ValidationImpl() {
-  return task_data->inputs_count[0] == task_data->outputs_count[0];
+  return task_data && task_data->inputs_count[0] == task_data->outputs_count[0];
 }
 
 bool karaseva_e_reduce_mpi::TestTaskMPI::RunImpl() {
@@ -30,7 +38,7 @@ bool karaseva_e_reduce_mpi::TestTaskMPI::RunImpl() {
 }
 
 void karaseva_e_reduce_mpi::TestTaskMPI::ReduceBinaryTree(boost::mpi::communicator &world) {
-  boost::mpi::reduce(world, input_.data(), size_, output_.data(), std::plus<int>(), 0);
+  boost::mpi::reduce(world, input_.data(), size_, output_.data(), std::plus<>(), 0);
 
   if (world.rank() == 0) {
     std::cout << "Reduce completed on rank 0\n";
@@ -38,6 +46,15 @@ void karaseva_e_reduce_mpi::TestTaskMPI::ReduceBinaryTree(boost::mpi::communicat
 }
 
 bool karaseva_e_reduce_mpi::TestTaskMPI::PostProcessingImpl() {
-  std::copy(output_.begin(), output_.end(), reinterpret_cast<int *>(task_data->outputs[0]));
+  if (!task_data || task_data->outputs.empty() || !task_data->outputs[0]) {
+    return false;
+  }
+
+  auto *out_ptr = reinterpret_cast<int *>(task_data->outputs[0]);
+  if (!out_ptr) {
+    return false;
+  }
+
+  std::ranges::copy(output_, out_ptr);
   return true;
 }
