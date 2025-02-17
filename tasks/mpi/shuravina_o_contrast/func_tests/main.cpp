60a8a183c6450f747ced0014a2a2c023c2d8280b
shyravina19102004 <shyravina19102004@gmail.com>
2025-02-17 16:14:30 +0300

    6

--- tasks/mpi/shuravina_o_contrast/func_tests/main.cpp
+++ tasks/mpi/shuravina_o_contrast/func_tests/main.cpp
@@ -38,36 +38,6 @@ TEST(shuravina_o_contrast, test_min_max_values) {
   EXPECT_EQ(max_val, 255);
 }
 
-TEST(shuravina_o_contrast, test_uniform_distribution) {
-  constexpr size_t kCount = 256;
-
-  std::vector<uint8_t> in(kCount * kCount, 0);
-  std::vector<uint8_t> out(kCount * kCount, 0);
-
-  for (size_t i = 0; i < kCount; i++) {
-    for (size_t j = 0; j < kCount; j++) {
-      in[(i * kCount) + j] = static_cast<uint8_t>(i);
-    }
-  }
-
-  auto task_data_mpi = std::make_shared<ppc::core::TaskData>();
-  task_data_mpi->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
-  task_data_mpi->inputs_count.emplace_back(in.size());
-  task_data_mpi->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
-  task_data_mpi->outputs_count.emplace_back(out.size());
-
-  shuravina_o_contrast::TestTaskMPI test_task_mpi(task_data_mpi);
-  ASSERT_EQ(test_task_mpi.Validation(), true);
-  test_task_mpi.PreProcessing();
-  test_task_mpi.Run();
-  test_task_mpi.PostProcessing();
-
-  uint8_t min_val = *std::ranges::min_element(out.begin(), out.end());
-  uint8_t max_val = *std::ranges::max_element(out.begin(), out.end());
-  EXPECT_EQ(min_val, 0);
-  EXPECT_EQ(max_val, 255);
-}
-
 TEST(shuravina_o_contrast, test_random_values) {
   constexpr size_t kCount = 256;
 
--- tasks/seq/shuravina_o_contrast/func_tests/main.cpp
+++ tasks/seq/shuravina_o_contrast/func_tests/main.cpp
@@ -53,26 +53,3 @@ TEST(shuravina_o_contrast, test_contrast_stretching_min_intensity) {
     EXPECT_EQ(out[i], 0);
   }
 }
-
-TEST(shuravina_o_contrast, test_contrast_stretching_max_intensity) {
-  constexpr size_t kSize = 8;
-
-  std::vector<uint8_t> in(kSize * kSize, 255);
-  std::vector<uint8_t> out(kSize * kSize, 0);
-
-  auto task_data_seq = std::make_shared<ppc::core::TaskData>();
-  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
-  task_data_seq->inputs_count.emplace_back(in.size());
-  task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
-  task_data_seq->outputs_count.emplace_back(out.size());
-
-  shuravina_o_contrast::ContrastTaskSequential contrast_task_sequential(task_data_seq);
-  ASSERT_EQ(contrast_task_sequential.Validation(), true);
-  contrast_task_sequential.PreProcessing();
-  contrast_task_sequential.Run();
-  contrast_task_sequential.PostProcessing();
-
-  for (size_t i = 0; i < out.size(); ++i) {
-    EXPECT_EQ(out[i], 255);
-  }
-}
