1c921876d5a7afbddcd80d750c3961ae3d52c45c
kate <ka10nnn@yandex.ru>
2025-02-18 11:58:45 +0300

    add ordinary tests

--- tasks/mpi/budazhapova_e_count_freq_character/func_tests/count_freq_of_ch_func_tests_mpi.cpp
+++ tasks/mpi/budazhapova_e_count_freq_character/func_tests/count_freq_of_ch_func_tests_mpi.cpp
@@ -230,6 +230,90 @@ TEST(budazhapova_e_count_freq_chart_mpi, no_symb_in_string) {
 }
 TEST(budazhapova_e_count_freq_chart_mpi, ordinary_string) {
   boost::mpi::communicator world;
+  std::string global_str = "gdfhfsef";
+  std::vector<int> global_out(1, 0);
+  char symb = 'f';
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> task_data_par = std::make_shared<ppc::core::TaskData>();
+
+  if (world.rank() == 0) {
+    task_data_par->inputs.emplace_back(reinterpret_cast<uint8_t*>(global_str.data()));
+    task_data_par->inputs_count.emplace_back(global_str.size());
+    task_data_par->inputs.emplace_back(reinterpret_cast<uint8_t*>(&symb));
+    task_data_par->inputs_count.emplace_back(1);
+    task_data_par->outputs.emplace_back(reinterpret_cast<uint8_t*>(global_out.data()));
+    task_data_par->outputs_count.emplace_back(global_out.size());
+  }
+  budazhapova_e_count_freq_chart_mpi::TestMPITaskParallel test_mpi_task_parallel(task_data_par);
+  ASSERT_EQ(test_mpi_task_parallel.Validation(), true);
+  test_mpi_task_parallel.PreProcessing();
+  test_mpi_task_parallel.Run();
+  test_mpi_task_parallel.PostProcessing();
+  if (world.rank() == 0) {
+    // Create data
+    std::vector<int> reference_out(1, 0);
+    // Create TaskData
+    std::shared_ptr<ppc::core::TaskData> task_data_seq = std::make_shared<ppc::core::TaskData>();
+    task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t*>(global_str.data()));
+    task_data_seq->inputs_count.emplace_back(global_str.size());
+    task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t*>(&symb));
+    task_data_seq->inputs_count.emplace_back(1);
+    task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t*>(reference_out.data()));
+    task_data_seq->outputs_count.emplace_back(reference_out.size());
+
+    // Create Task
+    budazhapova_e_count_freq_chart_mpi::TestMPITaskSequential test_mpi_task_sequential(task_data_seq);
+    ASSERT_EQ(test_mpi_task_sequential.Validation(), true);
+    test_mpi_task_sequential.PreProcessing();
+    test_mpi_task_sequential.Run();
+    test_mpi_task_sequential.PostProcessing();
+    ASSERT_EQ(reference_out[0], global_out[0]);
+  }
+}
+TEST(budazhapova_e_count_freq_chart_mpi, ordinary_string_2) {
+  boost::mpi::communicator world;
+  std::string global_str = "ergtergdfgdgesr";
+  std::vector<int> global_out(1, 0);
+  char symb = 'r';
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> task_data_par = std::make_shared<ppc::core::TaskData>();
+
+  if (world.rank() == 0) {
+    task_data_par->inputs.emplace_back(reinterpret_cast<uint8_t*>(global_str.data()));
+    task_data_par->inputs_count.emplace_back(global_str.size());
+    task_data_par->inputs.emplace_back(reinterpret_cast<uint8_t*>(&symb));
+    task_data_par->inputs_count.emplace_back(1);
+    task_data_par->outputs.emplace_back(reinterpret_cast<uint8_t*>(global_out.data()));
+    task_data_par->outputs_count.emplace_back(global_out.size());
+  }
+  budazhapova_e_count_freq_chart_mpi::TestMPITaskParallel test_mpi_task_parallel(task_data_par);
+  ASSERT_EQ(test_mpi_task_parallel.Validation(), true);
+  test_mpi_task_parallel.PreProcessing();
+  test_mpi_task_parallel.Run();
+  test_mpi_task_parallel.PostProcessing();
+  if (world.rank() == 0) {
+    // Create data
+    std::vector<int> reference_out(1, 0);
+    // Create TaskData
+    std::shared_ptr<ppc::core::TaskData> task_data_seq = std::make_shared<ppc::core::TaskData>();
+    task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t*>(global_str.data()));
+    task_data_seq->inputs_count.emplace_back(global_str.size());
+    task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t*>(&symb));
+    task_data_seq->inputs_count.emplace_back(1);
+    task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t*>(reference_out.data()));
+    task_data_seq->outputs_count.emplace_back(reference_out.size());
+
+    // Create Task
+    budazhapova_e_count_freq_chart_mpi::TestMPITaskSequential test_mpi_task_sequential(task_data_seq);
+    ASSERT_EQ(test_mpi_task_sequential.Validation(), true);
+    test_mpi_task_sequential.PreProcessing();
+    test_mpi_task_sequential.Run();
+    test_mpi_task_sequential.PostProcessing();
+    ASSERT_EQ(reference_out[0], global_out[0]);
+  }
+}
+TEST(budazhapova_e_count_freq_chart_mpi, ordinary_string_3) {
+  boost::mpi::communicator world;
   std::string global_str = "gdfhjdsfghbehjfbhjsef gejhfgse jf segfsgefyusg df gefgsugfdjhfsef";
   std::vector<int> global_out(1, 0);
   char symb = 'f';
