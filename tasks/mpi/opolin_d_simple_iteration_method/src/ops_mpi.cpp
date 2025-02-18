b4614aad0e34c5ede2da163f3c1047d914fe8b2e
Dmitry Alekseevich <opolindmitry@gmail.com>
2025-02-18 21:44:54 +0300

    with serial

--- tasks/mpi/opolin_d_simple_iteration_method/src/ops_mpi.cpp
+++ tasks/mpi/opolin_d_simple_iteration_method/src/ops_mpi.cpp
@@ -5,6 +5,7 @@
 #include <boost/mpi/collectives/broadcast.hpp>
 #include <boost/mpi/collectives/gatherv.hpp>
 #include <boost/mpi/collectives/scatterv.hpp>
+#include <boost/serialization/vector.hpp>
 #include <cmath>
 #include <cstddef>
 #include <cstdint>
