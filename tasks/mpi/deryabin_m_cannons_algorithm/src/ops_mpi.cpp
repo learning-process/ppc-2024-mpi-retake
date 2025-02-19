3a04aeaa4321d565f2ca99b72f35d771563543ef
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2025-02-19 07:40:27 +0300

    Update ops_mpi.cpp

--- tasks/mpi/deryabin_m_cannons_algorithm/src/ops_mpi.cpp
+++ tasks/mpi/deryabin_m_cannons_algorithm/src/ops_mpi.cpp
@@ -268,9 +268,10 @@ void deryabin_m_cannons_algorithm_mpi::CannonsAlgorithmMPITaskParallel::PerformC
 bool deryabin_m_cannons_algorithm_mpi::CannonsAlgorithmMPITaskParallel::RunImpl() {
   if (world_.size() != 1 && world_.size() == pow((unsigned short)sqrt(world_.size()), 2) &&
       static_cast<unsigned short>(std::sqrt(static_cast<unsigned short>(input_matrix_A_.size()))) %
-         static_cast<unsigned short>(std::sqrt(world_.size()))) == 0) {
+          static_cast<unsigned short>(std::sqrt(world_.size()))) == 0) {
     PerformCannonAlgorithm();
-  } else {
+  }
+  else {
     HandleTrivialCase();
   }
   return true;
