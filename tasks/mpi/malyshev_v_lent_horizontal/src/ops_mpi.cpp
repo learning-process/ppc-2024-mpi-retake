b396d030736eb1e356945c38d31f016e1f35bf89
dangerulya <ardon232@mail.ru>
2025-02-20 02:04:12 +0300

    Update ops_mpi.cpp

--- tasks/mpi/malyshev_v_lent_horizontal/src/ops_mpi.cpp
+++ tasks/mpi/malyshev_v_lent_horizontal/src/ops_mpi.cpp
@@ -5,6 +5,7 @@
 #include <boost/mpi/collectives/gatherv.hpp>
 #include <boost/mpi/collectives/scatterv.hpp>
 #include <boost/mpi/communicator.hpp>
+#include <boost/serialization/vector.hpp>  // NOLINT
 #include <random>
 #include <vector>
 
