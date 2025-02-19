4ecbfb26dca2c9ca12ad91bd245363f832800168
shyravina19102004 <shyravina19102004@gmail.com>
2025-02-19 21:01:17 +0300

    a8

--- tasks/mpi/shuravina_o_contrast/perf_tests/main.cpp
+++ tasks/mpi/shuravina_o_contrast/perf_tests/main.cpp
@@ -39,7 +39,7 @@ TEST(shuravina_o_contrast_mpi, test_pipeline_run) {
 }
 
 TEST(shuravina_o_contrast_mpi, test_task_run) {
-  constexpr size_t kSize = 512;
+  constexpr size_t kSize = 4096;
   std::vector<uint8_t> in(kSize * kSize, 128);
   std::vector<uint8_t> out(kSize * kSize, 0);
 
@@ -52,7 +52,7 @@ TEST(shuravina_o_contrast_mpi, test_task_run) {
   auto test_task_mpi = std::make_shared<shuravina_o_contrast::ContrastTaskMPI>(task_data_mpi);
 
   auto perf_attr = std::make_shared<ppc::core::PerfAttr>();
-  perf_attr->num_running = 10;
+  perf_attr->num_running = 1000;
   const auto t0 = std::chrono::high_resolution_clock::now();
   perf_attr->current_timer = [&] {
     auto current_time_point = std::chrono::high_resolution_clock::now();
