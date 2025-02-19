479fc0b8e0f83ca32aaa610bc6f6003b0a31508e
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2025-02-19 13:29:06 +0300

    Update main.cpp

--- tasks/mpi/deryabin_m_cannons_algorithm/perf_tests/main.cpp
+++ tasks/mpi/deryabin_m_cannons_algorithm/perf_tests/main.cpp
@@ -36,7 +36,7 @@ TEST(deryabin_m_cannons_algorithm_mpi, test_pipeline_run_Mpi) {
       std::make_shared<deryabin_m_cannons_algorithm_mpi::CannonsAlgorithmMPITaskParallel>(task_data_mpi);
 
   auto perf_attr = std::make_shared<ppc::core::PerfAttr>();
-  perf_attr->num_running = 1;
+  perf_attr->num_running = 10;
   const auto t0 = std::chrono::high_resolution_clock::now();
   perf_attr->current_timer = [&] {
     auto current_time_point = std::chrono::high_resolution_clock::now();
@@ -77,7 +77,7 @@ TEST(deryabin_m_cannons_algorithm_mpi, test_task_run_Mpi) {
       std::make_shared<deryabin_m_cannons_algorithm_mpi::CannonsAlgorithmMPITaskParallel>(task_data_par);
 
   auto perf_attr = std::make_shared<ppc::core::PerfAttr>();
-  perf_attr->num_running = 1;
+  perf_attr->num_running = 10;
   const auto t0 = std::chrono::high_resolution_clock::now();
   perf_attr->current_timer = [&] {
     auto current_time_point = std::chrono::high_resolution_clock::now();
