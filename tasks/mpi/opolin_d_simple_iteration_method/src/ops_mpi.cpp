f9a69d443a86975dd69904d6103a5e28d6e8cd5c
Dmitry Alekseevich <opolindmitry@gmail.com>
2025-02-19 12:44:01 +0300

    fix mpi

--- tasks/mpi/opolin_d_simple_iteration_method/src/ops_mpi.cpp
+++ tasks/mpi/opolin_d_simple_iteration_method/src/ops_mpi.cpp
@@ -5,6 +5,7 @@
 #include <boost/mpi/collectives/broadcast.hpp>
 #include <boost/mpi/collectives/gatherv.hpp>
 #include <boost/mpi/collectives/scatterv.hpp>
+#include <boost/serialization/vector.hpp> // NOLINT(misc-include-cleaner)
 #include <cmath>
 #include <cstddef>
 #include <cstdint>
