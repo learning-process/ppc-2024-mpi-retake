0985c5f0c7e800550862f371ac982be350df25d4
Kate <ka10nnn@yandex.ru>
2025-02-20 14:48:11 +0300

    clang tidy

--- tasks/mpi/budazhapova_betcher_odd_even_merge_mpi/src/radix_sort_with_betcher_main.cpp
+++ tasks/mpi/budazhapova_betcher_odd_even_merge_mpi/src/radix_sort_with_betcher_main.cpp
@@ -48,51 +48,51 @@ void OddEvenMerge(std::vector<int>& local, std::vector<int>& received_data) {
   // received_data.assign(merged.begin() + local.size(), merged.end());
   received_data.assign(merged.begin() + static_cast<long>(local.size()), merged.end());
 }
-void SendAndReceive(boost::mpi::communicator& world_, int send_rank, int recv_rank, std::vector<int>& data) {
-  if (send_rank >= 0 && send_rank < world_.size()) {
-    world_.send(send_rank, world_.rank(), data);
+void SendAndReceive(boost::mpi::communicator& world, int send_rank, int recv_rank, std::vector<int>& data) {
+  if (send_rank >= 0 && send_rank < world.size()) {
+    world.send(send_rank, world_.rank(), data);
   }
   if (recv_rank >= 0 && recv_rank < world_.size()) {
-    world_.recv(recv_rank, recv_rank, data);
+    world.recv(recv_rank, recv_rank, data);
   }
 }
 
-void PerformOddEvenMerge(boost::mpi::communicator& world_, int neighbor_rank, std::vector<int>& local_data) {
+void PerformOddEvenMerge(boost::mpi::communicator& world, int neighbor_rank, std::vector<int>& local_data) {
   std::vector<int> received_data;
-  world_.recv(neighbor_rank, neighbor_rank, received_data);
-  OddEvenMerge(&received_data, &local_data);
-  world_.send(neighbor_rank, world_.rank(), received_data);
+  world.recv(neighbor_rank, neighbor_rank, received_data);
+  OddEvenMerge(received_data, local_data);
+  world.send(neighbor_rank, world.rank(), received_data);
 }
 
-void OddEvenSortPhase(boost::mpi::communicator& world_, int phase) {
-  int next_rank = world_.rank() + 1;
-  int prev_rank = world_.rank() - 1;
+void OddEvenSortPhase(boost::mpi::communicator& world, int phase) {
+  int next_rank = world.rank() + 1;
+  int prev_rank = world.rank() - 1;
 
   if (phase % 2 == 0) {
-    if (world_.rank() % 2 == 0 && next_rank < world_.size()) {
+    if (world.rank() % 2 == 0 && next_rank < world.size()) {
       SendAndReceive(next_rank, -1, local_res_);
-    } else if (world_.rank() % 2 == 1) {
+    } else if (world.rank() % 2 == 1) {
       PerformOddEvenMerge(prev_rank, local_res_);
     }
-    if (world_.rank() % 2 == 0 && next_rank < world_.size()) {
+    if (world.rank() % 2 == 0 && next_rank < world.size()) {
       SendAndReceive(-1, next_rank, local_res_);
     }
   } else {
-    if (world_.rank() % 2 == 1 && next_rank < world_.size()) {
+    if (world.rank() % 2 == 1 && next_rank < world.size()) {
       SendAndReceive(next_rank, -1, local_res_);
-    } else if (world_.rank() % 2 == 0 && world_.rank() > 0) {
+    } else if (world.rank() % 2 == 0 && world.rank() > 0) {
       PerformOddEvenMerge(prev_rank, local_res_);
     }
-    if (world_.rank() % 2 == 1 && next_rank < world_.size()) {
+    if (world.rank() % 2 == 1 && next_rank < world.size()) {
       SendAndReceive(-1, next_rank, local_res_);
     }
   }
 }
 
-void DistributeData(boost::mpi::communicator& world_, int& n_of_send_elements, int& n_of_proc_with_extra_elements,
+void DistributeData(boost::mpi::communicator& world, int& n_of_send_elements, int& n_of_proc_with_extra_elements,
                     int& start, int& end, std::vector<int>& recv_counts, std::vector<int>& displacements) {
-  int world_size = world_.size();
-  int world_rank = world_.rank();
+  int world_size = world.size();
+  int world_rank = world.rank();
   int res_size = static_cast<int>(res_.size());
 
   n_of_send_elements = res_size / world_size;
@@ -177,7 +177,6 @@ bool budazhapova_betcher_odd_even_merge_mpi::MergeParallel::RunImpl() {
 
 bool budazhapova_betcher_odd_even_merge_mpi::MergeParallel::PostProcessingImpl() {
   if (world_.rank() == 0) {
-    // budazhapova_betcher_odd_even_merge_mpi::RadixSort(res_);
     int* output = reinterpret_cast<int*>(task_data->outputs[0]);
     for (size_t i = 0; i < res_.size(); i++) {
       output[i] = res_[i];
