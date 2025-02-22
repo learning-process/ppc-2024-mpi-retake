8819becd4f5e89457f904d4389c79b05b302d126
Denis Tarakanov <de.tarakanov@yadro.com>
2025-02-22 19:53:24 +0300

    feature: first sequential version

---
+++ tasks/seq/tarakanov_d_integration_the_trapezoid_method/include/ops_seq.hpp
@@ -0,0 +1,25 @@
+// Copyright 2025 Tarakanov Denis
+#pragma once
+
+#include <vector>
+
+#include "core/task/include/task.hpp"
+
+namespace tarakanov_d_integration_the_trapezoid_method_seq {
+
+class IntegrationTheTrapezoidMethodSequential : public ppc::core::Task {
+ public:
+  explicit IntegrationTheTrapezoidMethodSequential(std::shared_ptr<ppc::core::TaskData> taskData_)
+      : Task(std::move(taskData_)) {}
+  bool PreProcessingImpl() override;
+  bool ValidationImpl() override;
+  bool RunImpl() override;
+  bool PostProcessingImpl() override;
+
+ private:
+  double a{}, b{}, h{}, res{};
+
+  static double func_to_integrate(double x) { return x / 2; };
+};
+
+}  // namespace tarakanov_d_integration_the_trapezoid_method_seq
---
+++ tasks/seq/tarakanov_d_integration_the_trapezoid_method/src/ops_seq.cpp
@@ -0,0 +1,38 @@
+// Copyright 2025 Tarakanov Denis
+#include "seq/tarakanov_d_integration_the_trapezoid_method/include/ops_seq.hpp"
+
+bool tarakanov_d_integration_the_trapezoid_method_seq::IntegrationTheTrapezoidMethodSequential::PreProcessingImpl() {
+  // Init value for input and output
+  a = *reinterpret_cast<double*>(task_data->inputs[0]);
+  b = *reinterpret_cast<double*>(task_data->inputs[1]);
+  h = *reinterpret_cast<double*>(task_data->inputs[2]);
+  res = 0.0;
+  
+  return true;
+}
+
+bool tarakanov_d_integration_the_trapezoid_method_seq::IntegrationTheTrapezoidMethodSequential::ValidationImpl() {
+  bool res = task_data->inputs_count[0] == 3 && task_data->outputs_count[2] > 0 && task_data->outputs_count[0] == 1;
+
+  return res;
+}
+
+bool tarakanov_d_integration_the_trapezoid_method_seq::IntegrationTheTrapezoidMethodSequential::RunImpl() {
+  int n = static_cast<int>((b - a) / h);
+  double integral = 0.0;
+
+  for (int i = 0; i < n - 1; ++i) {
+    double x0 = a + i * h;
+    double x1 = a + (i + 1) * h;
+    integral += 0.5 * (func_to_integrate(x0) + func_to_integrate(x1)) * h;
+  }
+
+  res = integral;
+
+  return true;
+}
+
+bool tarakanov_d_integration_the_trapezoid_method_seq::IntegrationTheTrapezoidMethodSequential::PostProcessingImpl() {
+  *reinterpret_cast<double*>(task_data->outputs[0]) = res;
+  return true;
+}
