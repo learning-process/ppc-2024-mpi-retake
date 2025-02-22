ad9ce78fed75c71ddfcacea324d95fd22b71c1cf
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2025-02-22 16:26:42 +0300

    Update main.cpp

--- tasks/mpi/deryabin_m_cannons_algorithm/perf_tests/main.cpp
+++ tasks/mpi/deryabin_m_cannons_algorithm/perf_tests/main.cpp
@@ -1,9 +1,11 @@
 #include <gtest/gtest.h>
 
+#include <boost/mpi/communicator.hpp>
 #include <chrono>
 #include <cstddef>
 #include <cstdint>
 #include <memory>
+#include <random>
 #include <vector>
 
 #include "core/perf/include/perf.hpp"
@@ -11,19 +13,18 @@
 #include "mpi/deryabin_m_cannons_algorithm/include/ops_mpi.hpp"
 
 TEST(deryabin_m_cannons_algorithm_mpi, test_pipeline_run_Mpi) {
+  boost::mpi::communicator world;
   constexpr size_t kMatrixSize = 500;
-  std::vector<double> input_matrix_a = std::vector<double>(kMatrixSize * kMatrixSize, 0);
-  std::vector<double> input_matrix_b = std::vector<double>(kMatrixSize * kMatrixSize, 0);
-  std::vector<double> output_matrix_c = std::vector<double>(kMatrixSize * kMatrixSize, 0);
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_real_distribution<> distribution(-100, 100);
+  std::vector<double> input_matrix_a(kMatrixSize * kMatrixSize, distribution(gen));
+  std::vector<double> input_matrix_b(kMatrixSize * kMatrixSize, distribution(gen));
+  std::vector<double> output_matrix_c(kMatrixSize * kMatrixSize, 0);
   std::vector<std::vector<double>> out_matrix_c(1, output_matrix_c);
   std::vector<double> true_solution = std::vector<double>(kMatrixSize * kMatrixSize, 0);
-  for (unsigned short i = 0; i < kMatrixSize; i++) {
-    for (unsigned short j = 0; j < kMatrixSize; j++) {
-      input_matrix_a[j + (i * kMatrixSize)] = i + 1;
-      input_matrix_b[j + (i * kMatrixSize)] = j + 1;
-      true_solution[j + (i * kMatrixSize)] = (i + 1) * (j + 1) * (double)kMatrixSize;
-    }
-  }
+  std::vector<std::vector<double>> true_sol(1, true_solution);
+  
   auto task_data_mpi = std::make_shared<ppc::core::TaskData>();
   task_data_mpi->inputs.emplace_back(reinterpret_cast<uint8_t*>(input_matrix_a.data()));
   task_data_mpi->inputs.emplace_back(reinterpret_cast<uint8_t*>(input_matrix_b.data()));
@@ -31,6 +32,16 @@ TEST(deryabin_m_cannons_algorithm_mpi, test_pipeline_run_Mpi) {
   task_data_mpi->inputs_count.emplace_back(input_matrix_b.size());
   task_data_mpi->outputs.emplace_back(reinterpret_cast<uint8_t*>(out_matrix_c.data()));
   task_data_mpi->outputs_count.emplace_back(out_matrix_c.size());
+  
+  if (world.rank() == 0) {
+    auto task_data_seq = std::make_shared<ppc::core::TaskData>();
+    task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t*>(input_matrix_a.data()));
+    task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t*>(input_matrix_b.data()));
+    task_data_seq->inputs_count.emplace_back(input_matrix_a.size());
+    task_data_seq->inputs_count.emplace_back(input_matrix_b.size());
+    task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t*>(true_sol.data()));
+    task_data_seq->outputs_count.emplace_back(true_sol.size());
+  }
 
   auto test_mpi_task_parallel =
       std::make_shared<deryabin_m_cannons_algorithm_mpi::CannonsAlgorithmMPITaskParallel>(task_data_mpi);
@@ -48,33 +59,42 @@ TEST(deryabin_m_cannons_algorithm_mpi, test_pipeline_run_Mpi) {
   auto perf_analyzer = std::make_shared<ppc::core::Perf>(test_mpi_task_parallel);
   perf_analyzer->PipelineRun(perf_attr, perf_results);
   ppc::core::Perf::PrintPerfStatistic(perf_results);
-  ASSERT_EQ(true_solution, out_matrix_c[0]);
+  ASSERT_EQ(true_sol[0], out_matrix_c[0]);
 }
 
 TEST(deryabin_m_cannons_algorithm_mpi, test_task_run_Mpi) {
+  boost::mpi::communicator world;
   constexpr size_t kMatrixSize = 500;
-  std::vector<double> input_matrix_a1 = std::vector<double>(kMatrixSize * kMatrixSize, 0);
-  std::vector<double> input_matrix_b1 = std::vector<double>(kMatrixSize * kMatrixSize, 0);
-  std::vector<double> output_matrix_c1 = std::vector<double>(kMatrixSize * kMatrixSize, 0);
-  std::vector<std::vector<double>> out_matrix_c1(1, output_matrix_c1);
-  std::vector<double> true_solution1 = std::vector<double>(kMatrixSize * kMatrixSize, 0);
-  for (unsigned short i = 0; i < kMatrixSize; i++) {
-    for (unsigned short j = 0; j < kMatrixSize; j++) {
-      input_matrix_a1[j + (i * kMatrixSize)] = i + 1;
-      input_matrix_b1[j + (i * kMatrixSize)] = j + 1;
-      true_solution1[j + (i * kMatrixSize)] = (i + 1) * (j + 1) * (double)kMatrixSize;
-    }
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_real_distribution<> distribution(-100, 100);
+  std::vector<double> input_matrix_a(kMatrixSize * kMatrixSize, distribution(gen));
+  std::vector<double> input_matrix_b(kMatrixSize * kMatrixSize, distribution(gen));
+  std::vector<double> output_matrix_c(kMatrixSize * kMatrixSize, 0);
+  std::vector<std::vector<double>> out_matrix_c(1, output_matrix_c);
+  std::vector<double> true_solution = std::vector<double>(kMatrixSize * kMatrixSize, 0);
+  std::vector<std::vector<double>> true_sol(1, true_solution);
+  
+  auto task_data_mpi = std::make_shared<ppc::core::TaskData>();
+  task_data_mpi->inputs.emplace_back(reinterpret_cast<uint8_t*>(input_matrix_a.data()));
+  task_data_mpi->inputs.emplace_back(reinterpret_cast<uint8_t*>(input_matrix_b.data()));
+  task_data_mpi->inputs_count.emplace_back(input_matrix_a.size());
+  task_data_mpi->inputs_count.emplace_back(input_matrix_b.size());
+  task_data_mpi->outputs.emplace_back(reinterpret_cast<uint8_t*>(out_matrix_c.data()));
+  task_data_mpi->outputs_count.emplace_back(out_matrix_c.size());
+  
+  if (world.rank() == 0) {
+    auto task_data_seq = std::make_shared<ppc::core::TaskData>();
+    task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t*>(input_matrix_a.data()));
+    task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t*>(input_matrix_b.data()));
+    task_data_seq->inputs_count.emplace_back(input_matrix_a.size());
+    task_data_seq->inputs_count.emplace_back(input_matrix_b.size());
+    task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t*>(true_sol.data()));
+    task_data_seq->outputs_count.emplace_back(true_sol.size());
   }
-  auto task_data_par = std::make_shared<ppc::core::TaskData>();
-  task_data_par->inputs.emplace_back(reinterpret_cast<uint8_t*>(input_matrix_a1.data()));
-  task_data_par->inputs.emplace_back(reinterpret_cast<uint8_t*>(input_matrix_b1.data()));
-  task_data_par->inputs_count.emplace_back(input_matrix_a1.size());
-  task_data_par->inputs_count.emplace_back(input_matrix_b1.size());
-  task_data_par->outputs.emplace_back(reinterpret_cast<uint8_t*>(out_matrix_c1.data()));
-  task_data_par->outputs_count.emplace_back(out_matrix_c1.size());
 
-  auto test_task_par =
-      std::make_shared<deryabin_m_cannons_algorithm_mpi::CannonsAlgorithmMPITaskParallel>(task_data_par);
+  auto test_mpi_task_parallel =
+      std::make_shared<deryabin_m_cannons_algorithm_mpi::CannonsAlgorithmMPITaskParallel>(task_data_mpi);
 
   auto perf_attr = std::make_shared<ppc::core::PerfAttr>();
   perf_attr->num_running = 10;
@@ -86,8 +106,8 @@ TEST(deryabin_m_cannons_algorithm_mpi, test_task_run_Mpi) {
   };
 
   auto perf_results = std::make_shared<ppc::core::PerfResults>();
-  auto perf_analyzer = std::make_shared<ppc::core::Perf>(test_task_par);
+  auto perf_analyzer = std::make_shared<ppc::core::Perf>(test_mpi_task_parallel);
   perf_analyzer->TaskRun(perf_attr, perf_results);
   ppc::core::Perf::PrintPerfStatistic(perf_results);
-  ASSERT_EQ(true_solution1, out_matrix_c1[0]);
+  ASSERT_EQ(true_sol[0], out_matrix_c[0]);
 }
