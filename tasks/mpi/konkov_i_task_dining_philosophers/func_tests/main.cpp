90cbd7305e6c71a372f9547d6b0087d2bc71d445
keshaproger <125856206+keshaproger@users.noreply.github.com>
2025-02-24 01:23:48 +0300

    fix new 5

--- tasks/mpi/konkov_i_task_dining_philosophers/func_tests/main.cpp
+++ tasks/mpi/konkov_i_task_dining_philosophers/func_tests/main.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include <mpi.h>
+
 #include "mpi/konkov_i_task_dining_philosophers/include/ops_mpi.hpp"
 
 TEST(konkov_i_DiningPhilosophersTest, ValidNumberOfPhilosophers) {
