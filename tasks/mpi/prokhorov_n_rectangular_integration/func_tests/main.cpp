60a28e3ea42fdca46e8c14680699e65fa4ee92d3
Nikita <nikita200604prochorov@mail.ru>
2025-02-24 01:21:40 +0300

    fix_17

--- tasks/mpi/prokhorov_n_rectangular_integration/func_tests/main.cpp
+++ tasks/mpi/prokhorov_n_rectangular_integration/func_tests/main.cpp
@@ -50,9 +50,9 @@ TEST(prokhorov_n_rectangular_integration_mpi, test_integration_x_cubed) {
   }
 }
 
-TEST(prokhorov_n_rectangular_integration_mpi, test_integration_one_over_x) {
+TEST(prokhorov_n_rectangular_integration_mpi, test_integration_x_squared) {
   boost::mpi::communicator world;
-  std::vector<double> global_input = {1.0, 2.0, 1000.0};
+  std::vector<double> global_input = {0.0, 1.0, 1000.0};
   std::vector<double> global_result(1, 0.0);
 
   auto task_data_par = std::make_shared<ppc::core::TaskData>();
@@ -62,12 +62,13 @@ TEST(prokhorov_n_rectangular_integration_mpi, test_integration_one_over_x) {
   task_data_par->outputs_count.emplace_back(global_result.size());
 
   auto test_mpi_task_parallel = std::make_shared<prokhorov_n_rectangular_integration_mpi::TestTaskMPI>(task_data_par);
-  test_mpi_task_parallel->SetFunction([](double x) { return 1.0 / x; });
+  test_mpi_task_parallel->SetFunction([](double x) { return x * x; });
 
   ASSERT_TRUE(test_mpi_task_parallel->ValidationImpl());
-  ASSERT_TRUE(test_mpi_task_parallel->PreProcessingImpl());
-  ASSERT_TRUE(test_mpi_task_parallel->RunImpl());
-  ASSERT_TRUE(test_mpi_task_parallel->PostProcessingImpl());
+
+  test_mpi_task_parallel->PreProcessingImpl();
+  test_mpi_task_parallel->RunImpl();
+  test_mpi_task_parallel->PostProcessingImpl();
 
   if (world.rank() == 0) {
     std::vector<double> reference_result(1, 0.0);
@@ -80,13 +81,19 @@ TEST(prokhorov_n_rectangular_integration_mpi, test_integration_one_over_x) {
 
     auto test_mpi_task_sequential =
         std::make_shared<prokhorov_n_rectangular_integration_mpi::TestTaskSequential>(task_data_seq);
-    test_mpi_task_sequential->SetFunction([](double x) { return 1.0 / x; });
+    test_mpi_task_sequential->SetFunction([](double x) { return x * x; });
 
     ASSERT_TRUE(test_mpi_task_sequential->ValidationImpl());
-    ASSERT_TRUE(test_mpi_task_sequential->PreProcessingImpl());
-    ASSERT_TRUE(test_mpi_task_sequential->RunImpl());
-    ASSERT_TRUE(test_mpi_task_sequential->PostProcessingImpl());
+
+    test_mpi_task_sequential->PreProcessingImpl();
+    test_mpi_task_sequential->RunImpl();
+    test_mpi_task_sequential->PostProcessingImpl();
+
+    double a = global_input[0];
+    double b = global_input[1];
+    double expected_result = (b * b * b / 3.0) - (a * a * a / 3.0);
 
     ASSERT_NEAR(reference_result[0], global_result[0], 1e-3);
+    ASSERT_NEAR(global_result[0], expected_result, 1e-3);
   }
 }
