5c12210fe35c607622f5fd1577260f61e33ede7f
Kamal Muradov <muradoffk@gmail.com>
2025-02-24 21:39:46 +0300

    Seq implementation.

---
+++ tasks/seq/muradov_k_trapezoid_integral/func_tests/main.cpp
@@ -0,0 +1,46 @@
+#include <gtest/gtest.h>
+
+#include <cmath>
+#include <functional>
+
+#include "seq/muradov_k_trapezoid_integral/include/ops_seq.hpp"
+
+namespace muradov_k_trapezoid_integral_seq {
+
+TEST(muradov_k_trapezoid_integral_seq, SquareFunction) {
+  std::function<double(double)> f = [](double x) { return x * x; };
+  double a = 5.0;
+  double b = 10.0;
+  int n = 100;
+
+  double result = GetIntegralTrapezoidalRuleSequential(f, a, b, n);
+
+  double reference_sum = 0.0;
+  double h = (b - a) / n;
+  for (int i = 0; i < n; i++) {
+    double x_i = a + i * h;
+    double x_next = x_i + h;
+    reference_sum += (f(x_i) + f(x_next)) * 0.5 * h;
+  }
+  ASSERT_NEAR(reference_sum, result, 1e-6);
+}
+
+TEST(muradov_k_trapezoid_integral_seq, CubeFunction) {
+  std::function<double(double)> f = [](double x) { return x * x * x; };
+  double a = 0.0;
+  double b = 6.0;
+  int n = 100;
+
+  double result = GetIntegralTrapezoidalRuleSequential(f, a, b, n);
+
+  double reference_sum = 0.0;
+  double h = (b - a) / n;
+  for (int i = 0; i < n; i++) {
+    double x_i = a + i * h;
+    double x_next = x_i + h;
+    reference_sum += (f(x_i) + f(x_next)) * 0.5 * h;
+  }
+  ASSERT_NEAR(reference_sum, result, 1e-6);
+}
+
+}  // namespace muradov_k_trapezoid_integral_seq
---
+++ tasks/seq/muradov_k_trapezoid_integral/perf_tests/main.cpp
@@ -0,0 +1,42 @@
+#include <gtest/gtest.h>
+
+#include <chrono>
+#include <cmath>
+#include <functional>
+#include <iostream>
+
+#include "seq/muradov_k_trapezoid_integral/include/ops_seq.hpp"
+
+namespace muradov_k_trapezoid_integral_seq {
+
+TEST(muradov_k_trapezoid_integral_seq, test_task_run) {
+  std::function<double(double)> f = [](double x) { return x * sin(x); };
+  double a = 0.0;
+  double b = 10.0;
+  int n = 10000000;
+
+  auto start = std::chrono::high_resolution_clock::now();
+  double result = GetIntegralTrapezoidalRuleSequential(f, a, b, n);
+  auto end = std::chrono::high_resolution_clock::now();
+
+  std::chrono::duration<double> elapsed = end - start;
+  std::cout << "[SEQ Task Run] Result: " << result << ", Time: " << elapsed.count() << " seconds\n";
+  SUCCEED();
+}
+
+TEST(muradov_k_trapezoid_integral_seq, test_pipeline_run) {
+  std::function<double(double)> f = [](double x) { return exp(x); };
+  double a = -6.0;
+  double b = 6.0;
+  int n = 10000000;
+
+  auto start = std::chrono::high_resolution_clock::now();
+  double result = GetIntegralTrapezoidalRuleSequential(f, a, b, n);
+  auto end = std::chrono::high_resolution_clock::now();
+
+  std::chrono::duration<double> elapsed = end - start;
+  std::cout << "[SEQ Pipeline Run] Result: " << result << ", Time: " << elapsed.count() << " seconds\n";
+  SUCCEED();
+}
+
+}  // namespace muradov_k_trapezoid_integral_seq
