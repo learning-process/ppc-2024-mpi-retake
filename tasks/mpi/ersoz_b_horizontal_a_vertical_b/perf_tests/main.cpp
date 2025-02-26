ba1ec83afa94a234b66425457cf1f30a9f3a8e98
Berke Ersoz <145029854+berkeerenersoz@users.noreply.github.com>
2025-02-26 14:47:13 +0300

    removed main

--- tasks/mpi/ersoz_b_horizontal_a_vertical_b/perf_tests/main.cpp
+++ tasks/mpi/ersoz_b_horizontal_a_vertical_b/perf_tests/main.cpp
@@ -57,11 +57,3 @@ TEST(ersoz_b_horizontal_a_vertical_b_mpi, test_task_run) {
   }
   SUCCEED();
 }
-
-int main(int argc, char** argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  MPI_Init(&argc, &argv);
-  int result = RUN_ALL_TESTS();
-  MPI_Finalize();
-  return result;
-}
