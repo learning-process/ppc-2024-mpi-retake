1d8b8f2dc925293eb882618d467ad36671cda869
Kamal Muradov <muradoffk@gmail.com>
2025-02-26 15:06:02 +0300

    refacotr perf tests

--- tasks/mpi/muradov_k_trapezoid_integral/perf_tests/main.cpp
+++ tasks/mpi/muradov_k_trapezoid_integral/perf_tests/main.cpp
@@ -1,9 +1,7 @@
 #include <gtest/gtest.h>
 
-#include <chrono>
 #include <cmath>
 #include <functional>
-#include <iostream>
 #ifndef OMPI_SKIP_MPICXX
 #define OMPI_SKIP_MPICXX 1
 #endif
@@ -14,25 +12,39 @@
 namespace muradov_k_trapezoid_integral_mpi {
 
 TEST(muradov_k_trapezoid_integral_mpi, test_task_run) {
-  int rank = 0;
+  int rank;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
   std::function<double(double)> f = [](double x) { return x * sin(x); };
+  double a = 0.0;
+  double b = 10.0;
+  int n = 10000000;
 
-  auto start = std::chrono::high_resolution_clock::now();
-  auto end = std::chrono::high_resolution_clock::now();
-  SUCCEED();
+  double result = GetIntegralTrapezoidalRuleParallel(f, a, b, n);
+
+  if (rank == 0) {
+    double expected_value = 9.09304866;
+    double tolerance = 1e-4;
+    ASSERT_NEAR(result, expected_value, tolerance);
+  }
 }
 
 TEST(muradov_k_trapezoid_integral_mpi, test_pipeline_run) {
-  int rank = 0;
+  int rank;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
   std::function<double(double)> f = [](double x) { return exp(x); };
+  double a = -6.0;
+  double b = 6.0;
+  int n = 10000000;
 
-  auto start = std::chrono::high_resolution_clock::now();
-  auto end = std::chrono::high_resolution_clock::now();
-  SUCCEED();
+  double result = GetIntegralTrapezoidalRuleParallel(f, a, b, n);
+
+  if (rank == 0) {
+    double expected_value = exp(6) - exp(-6);
+    double tolerance = 1e-4;
+    ASSERT_NEAR(result, expected_value, tolerance);
+  }
 }
 
 }  // namespace muradov_k_trapezoid_integral_mpi
--- tasks/seq/muradov_k_trapezoid_integral/perf_tests/main.cpp
+++ tasks/seq/muradov_k_trapezoid_integral/perf_tests/main.cpp
@@ -1,9 +1,7 @@
 #include <gtest/gtest.h>
 
-#include <chrono>
 #include <cmath>
 #include <functional>
-#include <iostream>
 
 #include "seq/muradov_k_trapezoid_integral/include/ops_seq.hpp"
 
@@ -11,20 +9,30 @@ namespace muradov_k_trapezoid_integral_seq {
 
 TEST(muradov_k_trapezoid_integral_seq, test_task_run) {
   std::function<double(double)> f = [](double x) { return x * sin(x); };
+  double a = 0.0;
+  double b = 10.0;
+  int n = 10000000;
 
-  auto start = std::chrono::high_resolution_clock::now();
-  auto end = std::chrono::high_resolution_clock::now();
+  double result = GetIntegralTrapezoidalRuleSequential(f, a, b, n);
 
-  SUCCEED();
+  double expected_value = 9.09304866;
+  double tolerance = 1e-4;
+
+  ASSERT_NEAR(result, expected_value, tolerance);
 }
 
 TEST(muradov_k_trapezoid_integral_seq, test_pipeline_run) {
   std::function<double(double)> f = [](double x) { return exp(x); };
+  double a = -6.0;
+  double b = 6.0;
+  int n = 10000000;
 
-  auto start = std::chrono::high_resolution_clock::now();
-  auto end = std::chrono::high_resolution_clock::now();
+  double result = GetIntegralTrapezoidalRuleSequential(f, a, b, n);
 
-  SUCCEED();
+  double expected_value = exp(6) - exp(-6);
+  double tolerance = 1e-4;
+
+  ASSERT_NEAR(result, expected_value, tolerance);
 }
 
 }  // namespace muradov_k_trapezoid_integral_seq
