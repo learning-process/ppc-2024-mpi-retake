6be2fbc3cf23db26e5cb99daefa492823fe53fc6
Denis Tarakanov <de.tarakanov@yadro.com>
2025-02-27 12:21:10 +0300

    feature: clang-tidy fixes

--- tasks/mpi/tarakanov_d_integration_the_trapezoid_method/include/ops_mpi.hpp
+++ tasks/mpi/tarakanov_d_integration_the_trapezoid_method/include/ops_mpi.hpp
@@ -1,4 +1,4 @@
-// Copyright 2025 Tarakanov Denis
+  // Copyright 2025 Tarakanov Denis
 #pragma once
 
 #include <boost/mpi/collectives.hpp>
@@ -6,21 +6,24 @@
 
 #include "core/task/include/task.hpp"
 
+#include <utility>
+#include <memory>
+
 namespace tarakanov_d_integration_the_trapezoid_method_mpi {
 
 class IntegrationTheTrapezoidMethodMPI : public ppc::core::Task {
  public:
-  explicit IntegrationTheTrapezoidMethodMPI(std::shared_ptr<ppc::core::TaskData> taskData_)
-      : Task(std::move(taskData_)) {}
+  explicit IntegrationTheTrapezoidMethodMPI(std::shared_ptr<ppc::core::TaskData> task_data)
+      : Task(std::move(task_data)) {}
   bool PreProcessingImpl() override;
   bool ValidationImpl() override;
   bool RunImpl() override;
   bool PostProcessingImpl() override;
 
  private:
-  double a{}, b{}, h{}, res{};
-  static double func_to_integrate(double x) { return x / 2; };
-  boost::mpi::communicator world;
+  double a_{}, b_{}, h_{}, res_{};
+  static double FuncToIntegrate(double x) { return x / 2; };
+  boost::mpi::communicator world_;
 };
 
 }  // namespace tarakanov_d_integration_the_trapezoid_method_mpi
