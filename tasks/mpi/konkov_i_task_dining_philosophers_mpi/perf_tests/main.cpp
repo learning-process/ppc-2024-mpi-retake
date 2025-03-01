ccd36762062ceb2bf3de18417a989b87fc7b638d
keshaproger <125856206+keshaproger@users.noreply.github.com>
2025-03-02 02:47:31 +0300

    back perf

--- tasks/mpi/konkov_i_task_dining_philosophers_mpi/perf_tests/main.cpp
+++ tasks/mpi/konkov_i_task_dining_philosophers_mpi/perf_tests/main.cpp
@@ -1,34 +1,63 @@
+#include <gtest/gtest.h>
+
+#include <chrono>
 #include <memory>
 
 #include "boost/mpi/communicator.hpp"
+#include "core/perf/include/perf.hpp"
 #include "core/task/include/task.hpp"
-#include "gtest/gtest.h"
 #include "mpi/konkov_i_task_dining_philosophers_mpi/include/ops_mpi.hpp"
 
 TEST(DiningPhilosophersMPIPerfTest, test_pipeline_run_mpi) {
   boost::mpi::communicator world;
-  if (world.rank() == 0) {
-    ppc::core::TaskDataPtr task_data = std::make_shared<ppc::core::TaskData>();
-    task_data->inputs_count = {100};
 
-    konkov_i_task_dining_philosophers_mpi::DiningPhilosophersMPI task(task_data);
-    task.PreProcessingImpl();
-    task.ValidationImpl();
-    task.RunImpl();
-    task.PostProcessingImpl();
+  auto task_data = std::make_shared<ppc::core::TaskData>();
+  task_data->inputs_count.push_back(world.size());
+
+  auto task = std::make_shared<konkov_i_task_dining_philosophers_mpi::DiningPhilosophersMPI>(task_data);
+
+  auto perf_attr = std::make_shared<ppc::core::PerfAttr>();
+  perf_attr->num_running = 10;
+  perf_attr->current_timer = [&] {
+    static auto start_time = std::chrono::high_resolution_clock::now();
+    auto current_time = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(current_time - start_time).count();
+    return static_cast<double>(duration) * 1e-9;
+  };
+
+  auto perf_results = std::make_shared<ppc::core::PerfResults>();
+
+  auto perf_analyzer = std::make_shared<ppc::core::Perf>(task);
+  perf_analyzer->PipelineRun(perf_attr, perf_results);
+
+  if (world.rank() == 0) {
+    ppc::core::Perf::PrintPerfStatistic(perf_results);
   }
 }
 
 TEST(DiningPhilosophersMPIPerfTest, test_task_run_mpi) {
   boost::mpi::communicator world;
-  if (world.rank() == 0) {
-    ppc::core::TaskDataPtr task_data = std::make_shared<ppc::core::TaskData>();
-    task_data->inputs_count = {200};
 
-    konkov_i_task_dining_philosophers_mpi::DiningPhilosophersMPI task(task_data);
-    task.PreProcessingImpl();
-    task.ValidationImpl();
-    task.RunImpl();
-    task.PostProcessingImpl();
+  auto task_data = std::make_shared<ppc::core::TaskData>();
+  task_data->inputs_count.push_back(world.size());
+
+  auto task = std::make_shared<konkov_i_task_dining_philosophers_mpi::DiningPhilosophersMPI>(task_data);
+
+  auto perf_attr = std::make_shared<ppc::core::PerfAttr>();
+  perf_attr->num_running = 10;
+  perf_attr->current_timer = [&] {
+    static auto start_time = std::chrono::high_resolution_clock::now();
+    auto current_time = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(current_time - start_time).count();
+    return static_cast<double>(duration) * 1e-9;
+  };
+
+  auto perf_results = std::make_shared<ppc::core::PerfResults>();
+
+  auto perf_analyzer = std::make_shared<ppc::core::Perf>(task);
+  perf_analyzer->TaskRun(perf_attr, perf_results);
+
+  if (world.rank() == 0) {
+    ppc::core::Perf::PrintPerfStatistic(perf_results);
   }
 }
