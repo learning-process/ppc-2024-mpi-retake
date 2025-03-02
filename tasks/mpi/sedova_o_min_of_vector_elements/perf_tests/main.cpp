4f2c9dc107def81b60a46e23982725101053f596
Sedova-Olga <vyaz-sedovaolga@mail.ru>
2025-03-02 17:55:58 +0300

    4

--- tasks/mpi/sedova_o_min_of_vector_elements/perf_tests/main.cpp
+++ tasks/mpi/sedova_o_min_of_vector_elements/perf_tests/main.cpp
@@ -9,21 +9,23 @@
 #include <vector>
 
 #include "boost/mpi/communicator.hpp"
-#include "core/perf/include/perf.hpp"
 #include "core/task/include/task.hpp"
+#include "core/perf/include/perf.hpp"
 #include "mpi/sedova_o_min_of_vector_elements/include/ops_mpi.hpp"
 
 TEST(sedova_o_min_of_vector_elements_mpi, test_pipeline_run) {
-  boost::mpi::communicator world_;
+  boost::mpi::communicator world;
   std::vector<std::vector<int>> global_matrix;
-  std::vector<int32_t> global_min(1, INT_MAX);
+  std::vector<int> global_min(1, INT_MAX);
   int ref = INT_MIN;
 
   // Create TaskData
   std::shared_ptr<ppc::core::TaskData> task_data_par = std::make_shared<ppc::core::TaskData>();
-  if (world_.rank() == 0) {
+
+  if (world.rank() == 0) {
     std::random_device dev;
     std::mt19937 gen(dev());
+
     int rows = 4000;
     int columns = 4000;
     int min = -500;
@@ -34,20 +36,19 @@ TEST(sedova_o_min_of_vector_elements_mpi, test_pipeline_run) {
     global_matrix[index / columns][index / rows] = ref;
 
     for (unsigned int i = 0; i < global_matrix.size(); i++)
-      task_data_par->inputs.emplace_back(reinterpret_cast<uint8_t *>(global_matrix[i].data()));
+      task_data_par->inputs.emplace_back(reinterpret_cast<uint8_t*>(global_matrix[i].data()));
     task_data_par->inputs_count.emplace_back(rows);
     task_data_par->inputs_count.emplace_back(columns);
 
-    task_data_par->outputs.emplace_back(reinterpret_cast<uint8_t *>(global_min.data()));
+    task_data_par->outputs.emplace_back(reinterpret_cast<uint8_t*>(global_min.data()));
     task_data_par->outputs_count.emplace_back(global_min.size());
   }
 
-  // Create Task
-  sedova_o_min_of_vector_elements_mpi::TestTaskMPI test_task_parallel(task_data_par);
-  ASSERT_EQ(test_task_parallel.ValidationImpl(), true);
-  test_task_parallel.PreProcessingImpl();
-  test_task_parallel.RunImpl();
-  test_task_parallel.PostProcessingImpl();
+  auto task_data_parallel = std::make_shared<sedova_o_min_of_vector_elements_mpi::TestTaskMPI>(task_data_par);
+  ASSERT_EQ(task_data_parallel->ValidationImpl(), true);
+  task_data_parallel->PreProcessingImpl();
+  task_data_parallel->RunImpl();
+  task_data_parallel->PostProcessingImpl();
 
   // Create Perf attributes
   auto perf_attr = std::make_shared<ppc::core::PerfAttr>();
@@ -59,25 +60,27 @@ TEST(sedova_o_min_of_vector_elements_mpi, test_pipeline_run) {
   auto perf_results = std::make_shared<ppc::core::PerfResults>();
 
   // Create Perf analyzer
-  auto perf_analyzer = std::make_shared<ppc::core::Perf>(test_task_parallel);
+  auto perf_analyzer = std::make_shared<ppc::core::Perf>(task_data_parallel);
   perf_analyzer->PipelineRun(perf_attr, perf_results);
-  if (world_.rank() == 0) {
+  if (world.rank() == 0) {
     ppc::core::Perf::PrintPerfStatistic(perf_results);
     ASSERT_EQ(ref, global_min[0]);
   }
 }
 
 TEST(sedova_o_min_of_vector_elements_mpi, test_task_run) {
-  boost::mpi::communicator world_;
+  boost::mpi::communicator world;
   std::vector<std::vector<int>> global_matrix;
-  std::vector<int32_t> global_min(1, INT_MAX);
+  std::vector<int> global_min(1, INT_MAX);
   int ref = INT_MIN;
 
   // Create TaskData
   std::shared_ptr<ppc::core::TaskData> task_data_par = std::make_shared<ppc::core::TaskData>();
-  if (world_.rank() == 0) {
+
+  if (world.rank() == 0) {
     std::random_device dev;
     std::mt19937 gen(dev());
+
     int rows = 4000;
     int columns = 4000;
     int min = -500;
@@ -88,20 +91,19 @@ TEST(sedova_o_min_of_vector_elements_mpi, test_task_run) {
     global_matrix[index / columns][index / rows] = ref;
 
     for (unsigned int i = 0; i < global_matrix.size(); i++)
-      task_data_par->inputs.emplace_back(reinterpret_cast<uint8_t *>(global_matrix[i].data()));
+      task_data_par->inputs.emplace_back(reinterpret_cast<uint8_t*>(global_matrix[i].data()));
     task_data_par->inputs_count.emplace_back(rows);
     task_data_par->inputs_count.emplace_back(columns);
 
-    task_data_par->outputs.emplace_back(reinterpret_cast<uint8_t *>(global_min.data()));
+    task_data_par->outputs.emplace_back(reinterpret_cast<uint8_t*>(global_min.data()));
     task_data_par->outputs_count.emplace_back(global_min.size());
   }
 
-  // Create Task
-  sedova_o_min_of_vector_elements_mpi::TestTaskMPI test_task_parallel(task_data_par);
-  ASSERT_EQ(test_task_parallel.ValidationImpl(), true);
-  test_task_parallel.PreProcessingImpl();
-  test_task_parallel.RunImpl();
-  test_task_parallel.PostProcessingImpl();
+  auto task_data_parallel = std::make_shared<sedova_o_min_of_vector_elements_mpi::TestTaskMPI>(task_data_par);
+  ASSERT_EQ(task_data_parallel->ValidationImpl(), true);
+  task_data_parallel->PreProcessingImpl();
+  task_data_parallel->RunImpl();
+  task_data_parallel->PostProcessingImpl();
 
   // Create Perf attributes
   auto perf_attr = std::make_shared<ppc::core::PerfAttr>();
@@ -113,9 +115,9 @@ TEST(sedova_o_min_of_vector_elements_mpi, test_task_run) {
   auto perf_results = std::make_shared<ppc::core::PerfResults>();
 
   // Create Perf analyzer
-  auto perf_analyzer = std::make_shared<ppc::core::Perf>(test_task_parallel);
+  auto perf_analyzer = std::make_shared<ppc::core::Perf>(task_data_parallel);
   perf_analyzer->TaskRun(perf_attr, perf_results);
-  if (world_.rank() == 0) {
+  if (world.rank() == 0) {
     ppc::core::Perf::PrintPerfStatistic(perf_results);
     ASSERT_EQ(ref, global_min[0]);
   }
