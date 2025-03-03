fb45c9a0c2e97c66170a6d848b698774f87b3ccf
NeiroYT <nekkk1.lnnn1@yandex.ru>
2025-03-03 16:20:44 +0300

    Random test

--- tasks/mpi/leontev_n_binary/func_tests/main.cpp
+++ tasks/mpi/leontev_n_binary/func_tests/main.cpp
@@ -6,6 +6,7 @@
 #include <cstdint>
 #include <cstdlib>
 #include <memory>
+#include <unordered_map>
 #include <vector>
 
 #include "core/task/include/task.hpp"
@@ -21,7 +22,91 @@ inline void TaskEmplacement(std::shared_ptr<ppc::core::TaskData>& task_data_par,
   task_data_par->outputs_count.emplace_back(rows);
   task_data_par->outputs_count.emplace_back(cols);
 }
+
+std::vector<uint8_t> GetRandomVector(size_t rows, size_t cols) {
+  std::vector<uint8_t> img(rows * cols);
+  for (size_t i = 0; i < img.size(); i++) {
+    img[i] = rand() % 2;
+  }
+  return img;
+}
+
+bool CompNotZero(uint32_t a, uint32_t b) {
+  if (a == 0) {
+    return false;
+  }
+  if (b == 0) {
+    return true;
+  }
+  return a < b;
+}
+
+std::vector<uint32_t> RunSeq(const std::vector<uint8_t>& image, size_t rows, size_t cols) {
+  std::vector<uint32_t> labels(rows * cols, 0);
+  std::unordered_map<uint32_t, uint32_t> label_equivalences;
+  uint32_t cur_label = 1;
+  for (size_t row = 0; row < rows; ++row) {
+    for (size_t col = 0; col < cols; ++col) {
+      if (image[(row * cols) + col] == 0) {
+        continue;
+      }
+      uint32_t label_b = (col > 0) ? labels[(row * cols) + col - 1] : 0;
+      uint32_t label_c = (row > 0) ? labels[((row - 1) * cols) + col] : 0;
+      uint32_t label_d = (row > 0 && col > 0) ? labels[((row - 1) * cols) + col - 1] : 0;
+
+      if (label_b == 0 && label_c == 0 && label_d == 0) {
+        labels[(row * cols) + col] = cur_label++;
+      } else {
+        uint32_t min_label = std::min({label_b, label_c, label_d}, CompNotZero);
+        labels[(row * cols) + col] = min_label;
+        for (uint32_t label : {label_b, label_c, label_d}) {
+          if (label != 0 && label != min_label) {
+            label_equivalences[std::max(label, min_label)] = std::min(label, min_label);
+          }
+        }
+      }
+    }
+  }
+  for (auto& label : labels) {
+    while (label_equivalences.contains(label)) {
+      label = label_equivalences[label];
+    }
+  }
+  std::vector<size_t> arrived((rows * cols) + 1, 0);
+  size_t cur_mark = 1;
+  for (size_t i = 0; i < rows * cols; i++) {
+    if (labels[i] != 0) {
+      if (arrived[labels[i]] != 0) {
+        labels[i] = arrived[labels[i]];
+      } else {
+        labels[i] = arrived[labels[i]] = cur_mark++;
+      }
+    }
+  }
+  return labels;
+}
 }  // namespace
+
+TEST(leontev_n_binary_mpi, random_test) {
+  boost::mpi::communicator world;
+  size_t rows = 10;
+  size_t cols = 10;
+  std::vector<uint8_t> img = GetRandomVector(rows, cols);
+  std::shared_ptr<ppc::core::TaskData> task_data_par = std::make_shared<ppc::core::TaskData>();
+  std::vector<uint32_t> actual(rows * cols);
+  if (world.rank() == 0) {
+    TaskEmplacement(task_data_par, img, rows, cols, actual);
+  }
+  leontev_n_binary_mpi::BinarySegmentsMPI binary_segments(task_data_par);
+  binary_segments.Validation();
+  binary_segments.PreProcessing();
+  binary_segments.Run();
+  binary_segments.PostProcessing();
+  if (world.rank() == 0) {
+    std::vector<uint32_t> expected = RunSeq(img, rows, cols);
+    ASSERT_EQ(actual, expected);
+  }
+}
 
 TEST(leontev_n_binary_mpi, input_1) {
   boost::mpi::communicator world;
