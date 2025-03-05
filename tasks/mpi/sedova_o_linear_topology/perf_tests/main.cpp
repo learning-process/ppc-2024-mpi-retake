bb2d8dad4a544813c5b6f22fdb7ddfbfb837231c
Sedova-Olga <vyaz-sedovaolga@mail.ru>
2025-03-05 10:07:28 +0300

    test

--- tasks/mpi/sedova_o_linear_topology/perf_tests/main.cpp
+++ tasks/mpi/sedova_o_linear_topology/perf_tests/main.cpp
@@ -1,9 +1,11 @@
 #include <gtest/gtest.h>
 
-#include <boost/mpi/collectives.hpp>
 #include <boost/mpi/timer.hpp>
+#include <climits>
+#include <cstddef>
 #include <cstdint>
 #include <memory>
+#include <random>
 #include <vector>
 
 #include "boost/mpi/communicator.hpp"
@@ -15,7 +17,10 @@ TEST(sedova_o_linear_topology_mpi, test_pipeline_run) {
   boost::mpi::communicator world;
   std::vector<int> input;
   bool result = false;
+
+  // Create TaskData
   std::shared_ptr<ppc::core::TaskData> task_data_par = std::make_shared<ppc::core::TaskData>();
+
   if (world.rank() == 0) {
     const int count = 5000;
     input = sedova_o_linear_topology_mpi::GetRandomVector(count);
@@ -25,11 +30,11 @@ TEST(sedova_o_linear_topology_mpi, test_pipeline_run) {
     task_data_par->outputs_count.emplace_back(1);
   }
 
-  sedova_o_linear_topology_mpi::TestTaskMPI test_task_parallel(task_data_par);
-  ASSERT_EQ(test_task_parallel.ValidationImpl(), true);
-  test_task_parallel.PreProcessingImpl();
-  test_task_parallel.RunImpl();
-  test_task_parallel.PostProcessingImpl();
+  auto task_data_parallel = std::make_shared<sedova_o_linear_topology_mpi::TestTaskMPI>(task_data_par);
+  ASSERT_EQ(task_data_parallel->ValidationImpl(), true);
+  task_data_parallel->PreProcessingImpl();
+  task_data_parallel->RunImpl();
+  task_data_parallel->PostProcessingImpl();
 
   // Create Perf attributes
   auto perf_attr = std::make_shared<ppc::core::PerfAttr>();
@@ -41,7 +46,7 @@ TEST(sedova_o_linear_topology_mpi, test_pipeline_run) {
   auto perf_results = std::make_shared<ppc::core::PerfResults>();
 
   // Create Perf analyzer
-  auto perf_analyzer = std::make_shared<ppc::core::Perf>(test_task_parallel);
+  auto perf_analyzer = std::make_shared<ppc::core::Perf>(task_data_parallel);
   perf_analyzer->PipelineRun(perf_attr, perf_results);
   if (world.rank() == 0) {
     ppc::core::Perf::PrintPerfStatistic(perf_results);
@@ -53,7 +58,10 @@ TEST(sedova_o_linear_topology_mpi, test_task_run) {
   boost::mpi::communicator world;
   std::vector<int> input;
   bool result = false;
+
+  // Create TaskData
   std::shared_ptr<ppc::core::TaskData> task_data_par = std::make_shared<ppc::core::TaskData>();
+
   if (world.rank() == 0) {
     const int count = 5000;
     input = sedova_o_linear_topology_mpi::GetRandomVector(count);
@@ -63,12 +71,13 @@ TEST(sedova_o_linear_topology_mpi, test_task_run) {
     task_data_par->outputs_count.emplace_back(1);
   }
 
-  sedova_o_linear_topology_mpi::TestTaskMPI test_task_parallel(task_data_par);
-  ASSERT_EQ(test_task_parallel.ValidationImpl(), true);
-  test_task_parallel.PreProcessingImpl();
-  test_task_parallel.RunImpl();
-  test_task_parallel.PostProcessingImpl();
+  auto task_data_parallel = std::make_shared<sedova_o_linear_topology_mpi::TestTaskMPI>(task_data_par);
+  ASSERT_EQ(task_data_parallel->ValidationImpl(), true);
+  task_data_parallel->PreProcessingImpl();
+  task_data_parallel->RunImpl();
+  task_data_parallel->PostProcessingImpl();
 
+  // Create Perf attributes
   auto perf_attr = std::make_shared<ppc::core::PerfAttr>();
   perf_attr->num_running = 10;
   const boost::mpi::timer current_timer;
@@ -78,7 +87,7 @@ TEST(sedova_o_linear_topology_mpi, test_task_run) {
   auto perf_results = std::make_shared<ppc::core::PerfResults>();
 
   // Create Perf analyzer
-  auto perf_analyzer = std::make_shared<ppc::core::Perf>(test_task_parallel);
+  auto perf_analyzer = std::make_shared<ppc::core::Perf>(task_data_parallel);
   perf_analyzer->TaskRun(perf_attr, perf_results);
   if (world.rank() == 0) {
     ppc::core::Perf::PrintPerfStatistic(perf_results);
