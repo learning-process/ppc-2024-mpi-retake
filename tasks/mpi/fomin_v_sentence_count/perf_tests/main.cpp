f8f920f0c7be595d4fdbd2da5ef1fe6232410c14
W-Snack <90415228+W-Snack@users.noreply.github.com>
2025-03-06 07:50:17 +0300

    unuesd

--- tasks/mpi/fomin_v_sentence_count/perf_tests/main.cpp
+++ tasks/mpi/fomin_v_sentence_count/perf_tests/main.cpp
@@ -13,7 +13,6 @@ TEST(fomin_v_sentence_count, test_parallel_pipeline_run) {
 
   // Create TaskData
   std::shared_ptr<ppc::core::TaskData> task_data_mpi = std::make_shared<ppc::core::TaskData>();
-  int text_size = 0;
 
   if (world.rank() == 0) {
     global_text = "Hello! How are you? I am fine.";
@@ -55,7 +54,6 @@ TEST(fomin_v_sentence_count, test_sequential_task_run) {
 
   // Create TaskData
   std::shared_ptr<ppc::core::TaskData> task_data_seq = std::make_shared<ppc::core::TaskData>();
-  int text_size = 0;
 
   if (world.rank() == 0) {
     global_text = "Hello! How are you? I am fine.";
