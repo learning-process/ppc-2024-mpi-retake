e713492041989bf0ed22f7cf26e24a43c449f913
W-Snack <90415228+W-Snack@users.noreply.github.com>
2025-03-06 08:22:41 +0300

    34

--- tasks/mpi/fomin_v_sentence_count/perf_tests/main.cpp
+++ tasks/mpi/fomin_v_sentence_count/perf_tests/main.cpp
@@ -1,12 +1,12 @@
 #include <gtest/gtest.h>
 
+#include <boost/mpi/timer.hpp>
 #include <chrono>
 #include <cstdint>
 #include <memory>
 #include <string>
 #include <vector>
 
-#include <boost/mpi/timer.hpp>
 #include "boost/mpi/communicator.hpp"
 #include "core/perf/include/perf.hpp"
 #include "core/task/include/task.hpp"
