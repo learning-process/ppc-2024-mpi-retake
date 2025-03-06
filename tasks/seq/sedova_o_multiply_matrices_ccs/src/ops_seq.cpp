445e425c200742b0d7fb51d66d05fc061bfeaa08
Sedova-Olga <vyaz-sedovaolga@mail.ru>
2025-03-06 20:33:03 +0300

    cf

--- tasks/seq/sedova_o_multiply_matrices_ccs/src/ops_seq.cpp
+++ tasks/seq/sedova_o_multiply_matrices_ccs/src/ops_seq.cpp
@@ -1,8 +1,8 @@
 ﻿#include "seq/sedova_o_multiply_matrices_ccs/include/ops_seq.hpp"
 
-#include <algorithm>
+#include <algorithm>  // NOLINT
 #include <cmath>
-#include <vector>
+#include <vector>     // NOLINT
 
 bool sedova_o_multiply_matrices_ccs_seq::TestTaskSequential::PreProcessingImpl() {
   rows_A_ = *reinterpret_cast<int*>(task_data->inputs[0]);
@@ -40,10 +40,10 @@ bool sedova_o_multiply_matrices_ccs_seq::TestTaskSequential::PreProcessingImpl()
 }
 
 bool sedova_o_multiply_matrices_ccs_seq::TestTaskSequential::ValidationImpl() {
-  int rows_a = *reinterpret_cast<int*>(task_data->inputs[0]);
-  int cols_a = *reinterpret_cast<int*>(task_data->inputs[1]);
-  int rows_b = *reinterpret_cast<int*>(task_data->inputs[2]);
-  int cols_b = *reinterpret_cast<int*>(task_data->inputs[3]);
+  int rows_a = *reinterpret_cast<int*>(task_data->inputs[0]);  // NOLINT
+  int cols_a = *reinterpret_cast<int*>(task_data->inputs[1]);  // NOLINT
+  int rows_b = *reinterpret_cast<int*>(task_data->inputs[2]);  // NOLINT
+  int cols_b = *reinterpret_cast<int*>(task_data->inputs[3]);  // NOLINT
 
   return rows_a > 0 && cols_a > 0 && rows_b > 0 && cols_b > 0 && cols_a == rows_b;
 }
@@ -62,16 +62,16 @@ bool sedova_o_multiply_matrices_ccs_seq::TestTaskSequential::RunImpl() {
     std::ranges::fill(x.begin(), x.end(), -1);
     std::ranges::fill(x_values.begin(), x_values.end(), 0.0);
 
-    for (int i = B_col_ptr_[col_b]; i < B_col_ptr_[col_b + 1]; ++i) {
-      int row_b = B_row_ind_[i];
+    for (int i = B_col_ptr_[col_b]; i < B_col_ptr_[col_b + 1]; ++i) {  // NOLINT
+      int row_b = B_row_ind_[i];                                       // NOLINT
       x[row_b] = i;
       x_values[row_b] = B_val_[i];
     }
 
     for (int col_a = 0; col_a < static_cast<int>(At_col_ptr_.size() - 1); ++col_a) {
       double sum = 0.0;
-      for (int i = At_col_ptr_[col_a]; i < At_col_ptr_[col_a + 1]; ++i) {
-        int row_a = At_row_ind_[i];
+      for (int i = At_col_ptr_[col_a]; i < At_col_ptr_[col_a + 1]; ++i) {  // NOLINT
+        int row_a = At_row_ind_[i];                                        // NOLINT
         if (x[row_a] != -1) {
           sum += At_val_[i] * x_values[row_a];
         }
