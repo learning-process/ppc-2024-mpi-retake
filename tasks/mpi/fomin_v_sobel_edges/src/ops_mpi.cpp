746ea67e56d4c039f48494d33c179d9c9659b4d0
W-Snack <90415228+W-Snack@users.noreply.github.com>
2025-03-07 22:27:59 +0300

    mf

--- tasks/mpi/fomin_v_sobel_edges/src/ops_mpi.cpp
+++ tasks/mpi/fomin_v_sobel_edges/src/ops_mpi.cpp
@@ -15,18 +15,14 @@ using namespace std::chrono_literals;
 
 bool fomin_v_sobel_edges::SobelEdgeDetectionMPI::PreProcessingImpl() {
   if (world.rank() == 0) {
-    width_ = task_data->inputs_count[0];
-    height_ = task_data->inputs_count[1];
-
-    input_image_.resize(width_ * height_);
-    uint8_t *input_data = task_data->inputs[0];
-    std::copy(input_data, input_data + width_ * height_, input_image_.begin());
+    input_image_ = std::vector<unsigned char>(static_cast<unsigned char *>(task_data->inputs[0]),
+                                              static_cast<unsigned char *>(task_data->inputs[0]) + width_ * height_);
   }
 
   boost::mpi::broadcast(world, width_, 0);
   boost::mpi::broadcast(world, height_, 0);
 
-  const int delta_height = height_ / world.size();
+  int delta_height = height_ / world.size();
   local_height_ = (world.rank() == world.size() - 1) ? height_ - delta_height * (world.size() - 1) : delta_height;
 
   local_input_image_.resize((local_height_ + 2) * width_, 0);
@@ -36,14 +32,18 @@ bool fomin_v_sobel_edges::SobelEdgeDetectionMPI::PreProcessingImpl() {
   std::vector<int> displacements(world.size(), 0);
 
   if (world.rank() == 0) {
-    for (int proc = 1; proc < world.size(); ++proc) {
-      displacements[proc] = proc * delta_height * width_;
+    send_counts.back() = local_height_ * width_;
+    for (int i = 1; i < world.size(); ++i) {
+      displacements[i] = displacements[i - 1] + send_counts[i - 1];
     }
-    send_counts.back() = (height_ - delta_height * (world.size() - 1)) * width_;
   }
 
-  boost::mpi::scatterv(world, world.rank() == 0 ? input_image_.data() : nullptr, send_counts, displacements,
-                       local_input_image_.data() + width_, local_height_ * width_, 0);
+  if (world.rank() == 0) {
+    boost::mpi::scatterv(world, input_image_.data(), send_counts, displacements, local_input_image_.data() + width_,
+                         local_height_ * width_, 0);
+  } else {
+    boost::mpi::scatterv(world, local_input_image_.data() + width_, local_height_ * width_, 0);
+  }
 
   return true;
 }
@@ -68,40 +68,26 @@ bool fomin_v_sobel_edges::SobelEdgeDetectionMPI::RunImpl() {
   const int Gy[3][3] = {{-1, -2, -1}, {0, 0, 0}, {1, 2, 1}};
 
   if (world.rank() > 0) {
-    if (world.rank() % 2 == 0) {
-      world.send(world.rank() - 1, 0, local_input_image_.data() + width_, width_);
-      world.recv(world.rank() - 1, 0, local_input_image_.data(), width_);
-    } else {
-      world.recv(world.rank() - 1, 0, local_input_image_.data(), width_);
-      world.send(world.rank() - 1, 0, local_input_image_.data() + width_, width_);
-    }
+    world.send(world.rank() - 1, 0, &local_input_image_[width_], width_);
+    world.recv(world.rank() - 1, 0, &local_input_image_[0], width_);
   }
-
   if (world.rank() < world.size() - 1) {
-    if (world.rank() % 2 == 0) {
-      world.send(world.rank() + 1, 0, local_input_image_.data() + local_height_ * width_, width_);
-      world.recv(world.rank() + 1, 0, local_input_image_.data() + (local_height_ + 1) * width_, width_);
-    } else {
-      world.recv(world.rank() + 1, 0, local_input_image_.data() + (local_height_ + 1) * width_, width_);
-      world.send(world.rank() + 1, 0, local_input_image_.data() + local_height_ * width_, width_);
-    }
+    world.send(world.rank() + 1, 0, &local_input_image_[local_height_ * width_], width_);
+    world.recv(world.rank() + 1, 0, &local_input_image_[(local_height_ + 1) * width_], width_);
   }
 
-  for (int y = 1; y < local_height_ + 1; ++y) {
+  for (int y = 1; y <= local_height_; ++y) {
     for (int x = 1; x < width_ - 1; ++x) {
-      int sumX = 0;
-      int sumY = 0;
-
+      int sumX = 0, sumY = 0;
       for (int i = -1; i <= 1; ++i) {
         for (int j = -1; j <= 1; ++j) {
-          int pixel = local_input_image_[(y + i) * width_ + (x + j)];
-          sumX += pixel * Gx[i + 1][j + 1];
-          sumY += pixel * Gy[i + 1][j + 1];
+          const int pos = (y + i) * width_ + (x + j);
+          sumX += local_input_image_[pos] * Gx[i + 1][j + 1];
+          sumY += local_input_image_[pos] * Gy[i + 1][j + 1];
         }
       }
-
-      int gradient = static_cast<int>(std::sqrt(sumX * sumX + sumY * sumY));
-      local_output_image_[(y - 1) * width_ + x] = static_cast<unsigned char>(std::min(gradient, 255));
+      const int gradient = std::min(static_cast<int>(std::sqrt(sumX * sumX + sumY * sumY)), 255);
+      local_output_image_[(y - 1) * width_ + x] = static_cast<unsigned char>(gradient);
     }
   }
 
@@ -109,16 +95,17 @@ bool fomin_v_sobel_edges::SobelEdgeDetectionMPI::RunImpl() {
 }
 
 bool fomin_v_sobel_edges::SobelEdgeDetectionMPI::PostProcessingImpl() {
+  if (height_ == 0 || width_ == 0) return true;
+
   std::vector<int> recv_counts(world.size(), 0);
-  int delta_height = height_ / world.size();
+  const int delta = height_ / world.size();
   for (int i = 0; i < world.size(); ++i) {
-    recv_counts[i] =
-        (i == world.size() - 1) ? (height_ - delta_height * (world.size() - 1)) * width_ : delta_height * width_;
+    recv_counts[i] = (i == world.size() - 1) ? (height_ - delta * i) * width_ : delta * width_;
   }
 
-  std::vector<int> displacements(world.size(), 0);
-  for (int proc = 1; proc < world.size(); ++proc) {
-    displacements[proc] = displacements[proc - 1] + recv_counts[proc - 1];
+  std::vector<int> displs(world.size(), 0);
+  for (int i = 1; i < world.size(); ++i) {
+    displs[i] = displs[i - 1] + recv_counts[i - 1];
   }
 
   if (world.rank() == 0) {
@@ -126,10 +113,10 @@ bool fomin_v_sobel_edges::SobelEdgeDetectionMPI::PostProcessingImpl() {
   }
 
   boost::mpi::gatherv(world, local_output_image_.data(), local_output_image_.size(), output_image_.data(), recv_counts,
-                      displacements, 0);
+                      displs, 0);
 
   if (world.rank() == 0) {
-    *reinterpret_cast<std::vector<unsigned char> *>(task_data->outputs[0]) = output_image_;
+    std::copy(output_image_.begin(), output_image_.end(), static_cast<unsigned char *>(task_data->outputs[0]));
   }
 
   return true;
