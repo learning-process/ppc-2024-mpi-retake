a5c8ac1c76d65bcbefc23913d1e21f8c8ce234f6
Berke Ersoz <145029854+berkeerenersoz@users.noreply.github.com>
2025-03-09 01:06:17 +0300

    rerun jobs

--- tasks/mpi/ersoz_b_horizontal_linear_filtering_gauss/perf_tests/main.cpp
+++ tasks/mpi/ersoz_b_horizontal_linear_filtering_gauss/perf_tests/main.cpp
@@ -13,7 +13,7 @@
 #include "core/task/include/task.hpp"
 #include "mpi/ersoz_b_horizontal_linear_filtering_gauss/include/ops_mpi.hpp"
 
-TEST(ersoz_b_test_task_mpi, test_pipeline_run) { 
+TEST(ersoz_b_test_task_mpi, test_pipeline_run) {
   constexpr int kN = 512;
   std::vector<char> in(kN * kN, 0);
   for (int i = 0; i < kN; i++) {
