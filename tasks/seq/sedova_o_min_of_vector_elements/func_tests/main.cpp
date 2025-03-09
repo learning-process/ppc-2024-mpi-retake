14d264434bf65d9295cdde0461dc92b6c76783ca
Sedova-Olga <vyaz-sedovaolga@mail.ru>
2025-03-09 14:35:23 +0300

    test

--- tasks/seq/sedova_o_min_of_vector_elements/func_tests/main.cpp
+++ tasks/seq/sedova_o_min_of_vector_elements/func_tests/main.cpp
@@ -105,12 +105,48 @@ TEST(sedova_o_min_of_vector_elements_seq, test_100x100) {
   ASSERT_EQ(ref, output[0]);
 }
 
-TEST(sedova_o_min_of_vector_elements_seq, test_100x50) {
+TEST(sedova_o_min_of_vector_elements_seq, test_0x0) {
   std::random_device dev;
   std::mt19937 gen(dev());
 
-  const int rows = 100;
-  const int columns = 50;
+  const int rows = 0;
+  const int columns = 0;
+  const int min = -500;
+  const int max = 500;
+
+  // Create data
+  std::vector<int> output(1, INT_MAX);
+  std::vector<std::vector<int>> input = sedova_o_min_of_vector_elements_seq::GetRandomMatrix(rows, columns, min, max);
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> task_data_seq = std::make_shared<ppc::core::TaskData>();
+
+  if (!input.empty()) {
+    for (unsigned int i = 0; i < input.size(); i++) {
+      task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t *>(input[i].data()));
+      task_data_seq->inputs_count.emplace_back(input[i].size());
+
+      task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t *>(output.data()));
+      task_data_seq->outputs_count.emplace_back(output.size());
+    }
+  } else {
+    task_data_seq->inputs_count.emplace_back(0);
+    task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t *>(output.data()));
+    task_data_seq->outputs_count.emplace_back(output.size());
+  }
+
+  // Create Task
+  sedova_o_min_of_vector_elements_seq::TestTaskSequential test_task_sequential(task_data_seq);
+  ASSERT_EQ(test_task_sequential.ValidationImpl(), false);
+}
+
+
+TEST(sedova_o_min_of_vector_elements_seq, test_1x1) {
+  std::random_device dev;
+  std::mt19937 gen(dev());
+
+  const int rows = 1;
+  const int columns = 1;
   const int min = -500;
   const int max = 500;
   int ref = INT_MIN;
