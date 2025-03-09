e142885d39e3f79d5ecb7b5199a09b3091e9dd38
sudo-technoir <skurinskayae@gmail.com>
2025-03-09 15:35:00 +0300

    Update main.cpp

--- tasks/mpi/shkurinskaya_e_fox_matrix_mult/func_tests/main.cpp
+++ tasks/mpi/shkurinskaya_e_fox_matrix_mult/func_tests/main.cpp
@@ -11,6 +11,10 @@
 #include "mpi/shkurinskaya_e_fox_matrix_mult/include/ops_sec.hpp"
 
 namespace shkurinskaya_e_fox_mat_mul_mpi {
+    std::vector<double> GetRandomMatrix(int rows, int cols);
+}
+
+namespace shkurinskaya_e_fox_mat_mul_mpi {
 std::vector<double> GetRandomMatrix(int rows, int cols) {
   std::vector<double> result(rows * cols);
 
