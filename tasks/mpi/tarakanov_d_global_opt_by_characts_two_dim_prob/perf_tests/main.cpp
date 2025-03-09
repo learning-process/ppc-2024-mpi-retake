982e52cb655f39a7eb879e7d7546c6a224082f60
Denis Tarakanov <de.tarakanov@yadro.com>
2025-03-09 21:33:35 +0300

    feature: Perf tests data is been defining randomly now

--- tasks/mpi/tarakanov_d_global_opt_by_characts_two_dim_prob/perf_tests/main.cpp
+++ tasks/mpi/tarakanov_d_global_opt_by_characts_two_dim_prob/perf_tests/main.cpp
@@ -4,6 +4,7 @@
 #include <boost/mpi/timer.hpp>
 #include <cstdint>
 #include <memory>
+#include <random>
 #include <vector>
 
 #include "core/perf/include/perf.hpp"
@@ -16,16 +17,31 @@
     double step = 0.3;                                                                                        \
     std::vector<double> area = {-10, 10, -10, 10};                                                            \
     std::vector<double> func = {5, 5};                                                                        \
-    std::vector<double> constraint = {1, 1, 1, 2, 1, 1, 2, 3, 1, 4, 1, 1, 1, 1, 1, 2, 1, 1,                   \
-                                      2, 3, 1, 4, 1, 1, 1, 1, 1, 2, 1, 1, 2, 3, 1, 4, 1, 1};                  \
     std::vector<double> out(1, 0);                                                                            \
     std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();               \
     if (world.rank() == 0) {                                                                                  \
+      int num_constraints = 12;                                                                               \
+      std::vector<double> constraint(num_constraints * 3);                                                    \
+      std::random_device rd;                                                                                  \
+      std::mt19937 gen(rd());                                                                                 \
+      std::uniform_real_distribution<> dis_ab(-1.0, 1.0);                                                     \
+      std::uniform_real_distribution<> dis_d(0.0, 10.0);                                                      \
+      double p = func[0];                                                                                     \
+      double q = func[1];                                                                                     \
+      for (int i = 0; i < num_constraints; i++) {                                                             \
+        double a = dis_ab(gen);                                                                               \
+        double b = dis_ab(gen);                                                                               \
+        double d = dis_d(gen);                                                                                \
+        double c = (a * p) + (b * q) + d;                                                                     \
+        constraint[i * 3] = a;                                                                                \
+        constraint[(i * 3) + 1] = b;                                                                          \
+        constraint[(i * 3) + 2] = c;                                                                          \
+      }                                                                                                       \
       taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(area.data()));                              \
       taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(func.data()));                              \
       taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(constraint.data()));                        \
       taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(&step));                                    \
-      taskDataPar->inputs_count.emplace_back(12);                                                             \
+      taskDataPar->inputs_count.emplace_back(num_constraints);                                                \
       taskDataPar->inputs_count.emplace_back(0);                                                              \
       taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(out.data()));                              \
       taskDataPar->outputs_count.emplace_back(out.size());                                                    \
@@ -40,7 +56,6 @@
     perfAnalyzer->runMethod(perfAttr, perfResults);                                                           \
     if (world.rank() == 0) {                                                                                  \
       ppc::core::Perf::PrintPerfStatistic(perfResults);                                                       \
-      EXPECT_NEAR(46.777778, out[0], 0.0001);                                                                 \
     }                                                                                                         \
   }
 
