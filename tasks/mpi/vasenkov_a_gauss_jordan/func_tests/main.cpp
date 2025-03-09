00c43a9789eaeb687ba9f0b90097c3fd9ec946f3
VasenkovAA <114131027+VasenkovAA@users.noreply.github.com>
2025-03-09 12:38:49 +0300

    fix tests

--- tasks/mpi/vasenkov_a_gauss_jordan/func_tests/main.cpp
+++ tasks/mpi/vasenkov_a_gauss_jordan/func_tests/main.cpp
@@ -9,8 +9,8 @@
 #include "core/task/include/task.hpp"
 #include "mpi/vasenkov_a_gauss_jordan/include/ops_mpi.hpp"
 
-namespace vasenkov_a_gauss_jordan_mpi{
-  static std::vector<double> GenerateRandomMatrix(int n, double min_value = -10.0, double max_value = 10.0) {
+namespace {
+static std::vector<double> GenerateRandomMatrix(int n, double min_value = -10.0, double max_value = 10.0) {
   std::random_device rd;
   std::mt19937 gen(rd());
   std::uniform_real_distribution<double> dist(min_value, max_value);
@@ -24,24 +24,6 @@ namespace vasenkov_a_gauss_jordan_mpi{
   }
 
   return matrix;
-}
-std::shared_ptr<ppc::core::TaskData> CreateTaskData(const std::vector<double>& matrix, int n, std::vector<double>& result) {
-  auto task_data = std::make_shared<ppc::core::TaskData>();
-
-  if (!matrix.empty()) {
-    task_data->inputs.emplace_back(reinterpret_cast<uint8_t *>(const_cast<double*>(matrix.data())));
-    task_data->inputs_count.emplace_back(matrix.size());
-  }
-
-  task_data->inputs.emplace_back(reinterpret_cast<uint8_t *>(&n));
-  task_data->inputs_count.emplace_back(1);
-
-  if (!result.empty()) {
-    task_data->outputs.emplace_back(reinterpret_cast<uint8_t *>(result.data()));
-    task_data->outputs_count.emplace_back(result.size());
-  }
-
-  return task_data;
 }
 
 static void RunSequentialVersion(const std::vector<double> &global_matrix, int n, std::vector<double> &seq_result) {
@@ -89,11 +71,10 @@ bool RunParallelVersion(const std::vector<double> &global_matrix, int n, std::ve
 
   return par_run_res;
 }
-}// namespace vasenkov_a_gauss_jordan_mpi
+}  // namespace
 
 TEST(vasenkov_a_gauss_jordan_mpi, three_simple_matrix) {
   boost::mpi::communicator world;
-
   std::vector<double> global_matrix;
   int n = 0;
   std::vector<double> global_result;
@@ -101,23 +82,14 @@ TEST(vasenkov_a_gauss_jordan_mpi, three_simple_matrix) {
   if (world.rank() == 0) {
     global_matrix = {1, 2, 1, 10, 4, 8, 3, 20, 2, 5, 9, 30};
     n = 3;
-
     global_result.resize(n * (n + 1));
   }
-
-  bool par_run_res = vasenkov_a_gauss_jordan_mpi::RunParallelVersion(global_matrix, n, global_result);
-
+  RunParallelVersion(global_matrix, n, global_result);
   if (world.rank() == 0) {
     std::vector<double> seq_result(global_result.size(), 0);
-
-    vasenkov_a_gauss_jordan_mpi::RunSequentialVersion(global_matrix, n, seq_result);
-
-    if (par_run_res) {
-      ASSERT_EQ(global_result.size(), seq_result.size());
-      EXPECT_EQ(global_result, seq_result);
-    } else {
-      EXPECT_FALSE(par_run_res);
-    }
+    RunSequentialVersion(global_matrix, n, seq_result);
+    ASSERT_EQ(global_result.size(), seq_result.size());
+    EXPECT_EQ(global_result, seq_result);
   }
 }
 
@@ -127,52 +99,34 @@ TEST(vasenkov_a_gauss_jordan_mpi, identity_matrix) {
   std::vector<double> global_matrix;
   int n = 0;
   std::vector<double> global_result;
-
   std::shared_ptr<ppc::core::TaskData> task_data_par = std::make_shared<ppc::core::TaskData>();
-
   if (world.rank() == 0) {
     global_matrix = {1, 0, 0, 1, 0, 1, 0, 2, 0, 0, 1, 3};
     n = 3;
-
     global_result.resize(n * (n + 1));
   }
-
-  bool par_run_res = vasenkov_a_gauss_jordan_mpi::RunParallelVersion(global_matrix, n, global_result);
-
+  RunParallelVersion(global_matrix, n, global_result);
   if (world.rank() == 0) {
     std::vector<double> seq_result(global_result.size(), 0);
-
-    vasenkov_a_gauss_jordan_mpi::RunSequentialVersion(global_matrix, n, seq_result);
-
-    if (par_run_res) {
-      ASSERT_EQ(global_result.size(), seq_result.size());
-      EXPECT_EQ(global_result, seq_result);
-    } else {
-      EXPECT_FALSE(par_run_res);
-    }
+    RunSequentialVersion(global_matrix, n, seq_result);
+    ASSERT_EQ(global_result.size(), seq_result.size());
+    EXPECT_EQ(global_result, seq_result);
   }
 }
 TEST(vasenkov_a_gauss_jordan_mpi, singular_matrix) {
   boost::mpi::communicator world;
-
   std::vector<double> global_matrix;
   int n = 0;
   std::vector<double> global_result;
-
   std::shared_ptr<ppc::core::TaskData> task_data_par = std::make_shared<ppc::core::TaskData>();
-
   if (world.rank() == 0) {
     global_matrix = {1, 2, 3, 4, 2, 4, 6, 8, 3, 6, 9, 12};
     n = 3;
-
     global_result.resize(n * (n + 1));
-
     task_data_par->inputs.emplace_back(reinterpret_cast<uint8_t *>(const_cast<double *>(global_matrix.data())));
     task_data_par->inputs_count.emplace_back(global_matrix.size());
-
     task_data_par->inputs.emplace_back(reinterpret_cast<uint8_t *>(&n));
     task_data_par->inputs_count.emplace_back(1);
-
     task_data_par->outputs.emplace_back(reinterpret_cast<uint8_t *>(global_result.data()));
     task_data_par->outputs_count.emplace_back(global_result.size());
   }
@@ -190,29 +144,18 @@ TEST(vasenkov_a_gauss_jordan_mpi, large_matrix) {
   std::vector<double> global_matrix;
   int n = 0;
   std::vector<double> global_result;
-
   std::shared_ptr<ppc::core::TaskData> task_data_par = std::make_shared<ppc::core::TaskData>();
-
   if (world.rank() == 0) {
     global_matrix = {2, 1, -1, 2, 1, -1, 1, 3, 3, -1, 2, 1, 1, 2, -1, 1, 8, 11, -5, 6};
     n = 4;
-
     global_result.resize(n * (n + 1));
   }
-
-  bool par_run_res = vasenkov_a_gauss_jordan_mpi::RunParallelVersion(global_matrix, n, global_result);
-
+  RunParallelVersion(global_matrix, n, global_result);
   if (world.rank() == 0) {
     std::vector<double> seq_result(global_result.size(), 0);
-
-    vasenkov_a_gauss_jordan_mpi:: RunSequentialVersion(global_matrix, n, seq_result);
-
-    if (par_run_res) {
-      ASSERT_EQ(global_result.size(), seq_result.size());
-      EXPECT_EQ(global_result, seq_result);
-    } else {
-      EXPECT_FALSE(par_run_res);
-    }
+    RunSequentialVersion(global_matrix, n, seq_result);
+    ASSERT_EQ(global_result.size(), seq_result.size());
+    EXPECT_EQ(global_result, seq_result);
   }
 }
 TEST(vasenkov_a_gauss_jordan_mpi, zero_matrix) {
@@ -250,31 +193,20 @@ TEST(vasenkov_a_gauss_jordan_mpi, zero_matrix) {
 
 TEST(vasenkov_a_gauss_jordan_mpi, random_matrix) {
   boost::mpi::communicator world;
-
   std::vector<double> global_matrix;
   int n = 0;
   std::vector<double> global_result;
-
   std::shared_ptr<ppc::core::TaskData> task_data_par = std::make_shared<ppc::core::TaskData>();
-
   if (world.rank() == 0) {
     n = 4;
-    global_matrix = vasenkov_a_gauss_jordan_mpi::GenerateRandomMatrix(n);
+    global_matrix = GenerateRandomMatrix(n);
     global_result.resize(n * (n + 1));
   }
-
-  bool par_run_res = vasenkov_a_gauss_jordan_mpi::RunParallelVersion(global_matrix, n, global_result);
-
+  RunParallelVersion(global_matrix, n, global_result);
   if (world.rank() == 0) {
     std::vector<double> seq_result(global_result.size(), 0);
-
-    vasenkov_a_gauss_jordan_mpi::RunSequentialVersion(global_matrix, n, seq_result);
-
-    if (par_run_res) {
-      ASSERT_EQ(global_result.size(), seq_result.size());
-      EXPECT_EQ(global_result, seq_result);
-    } else {
-      EXPECT_FALSE(par_run_res);
-    }
+    RunSequentialVersion(global_matrix, n, seq_result);
+    ASSERT_EQ(global_result.size(), seq_result.size());
+    EXPECT_EQ(global_result, seq_result);
   }
 }
