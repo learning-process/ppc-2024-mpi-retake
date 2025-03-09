d0050ff5e3c2b89bc1d8b4ae2b8f84bf388b6a88
Sedova-Olga <vyaz-sedovaolga@mail.ru>
2025-03-09 13:08:21 +0300

    test

--- tasks/mpi/sedova_o_min_of_vector_elements/func_tests/main.cpp
+++ tasks/mpi/sedova_o_min_of_vector_elements/func_tests/main.cpp
@@ -271,30 +271,6 @@ TEST(sedova_o_min_of_vector_elements_mpi, test_0x0) {
       task_data_par->outputs_count.emplace_back(output.size());
     }
   }
-
   sedova_o_min_of_vector_elements_mpi::TestTaskMPI test_task_parallel(task_data_par);
-  ASSERT_EQ(test_task_parallel.ValidationImpl(), true);
-  test_task_parallel.PreProcessingImpl();
-  test_task_parallel.RunImpl();
-  test_task_parallel.PostProcessingImpl();
-
-  if (world.rank() == 0) {
-    // Create data
-    std::vector<int> output1(1, INT_MAX);
-
-    // Create TaskData
-    std::shared_ptr<ppc::core::TaskData> task_data_seq = std::make_shared<ppc::core::TaskData>();
-    for (unsigned int i = 0; i < global_matrix.size(); i++) {
-      task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t *>(global_matrix[i].data()));
-      task_data_seq->inputs_count.emplace_back(rows);
-      task_data_seq->inputs_count.emplace_back(columns);
-
-      task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t *>(output1.data()));
-      task_data_seq->outputs_count.emplace_back(output1.size());
-    }
-
-    // Create Task
-    sedova_o_min_of_vector_elements_mpi::TestTaskSequential test_task_sequential(task_data_seq);
-    ASSERT_EQ(test_task_sequential.ValidationImpl(), false);
-  }
+  ASSERT_EQ(test_task_parallel.ValidationImpl(), false);
 }
