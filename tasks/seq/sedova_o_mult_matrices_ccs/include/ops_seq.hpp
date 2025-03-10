2794d3a90aa11dff24ad5cfac08453a3252378d8
Sedova-Olga <vyaz-sedovaolga@mail.ru>
2025-03-10 14:47:04 +0300

    test

--- tasks/seq/sedova_o_mult_matrices_ccs/include/ops_seq.hpp
+++ tasks/seq/sedova_o_mult_matrices_ccs/include/ops_seq.hpp
@@ -10,14 +10,14 @@ namespace sedova_o_test_task_seq {
 template <typename T>
 T MultVectors(const std::vector<T> &vector_A, const std::vector<T> &vector_B);
 std::pair<std::vector<std::vector<double>>, std::vector<std::vector<double>>> Convertirovanie(
-    const std::vector<double> &A, const std::vector<int> &row_ind_A, const std::vector<int> &col_ind_A,
-    const std::vector<double> &B, const std::vector<int> &row_ind_B, const std::vector<int> &col_ind_B, int rows_A,
+    const std::vector<double> &A, const std::vector<int> &row_in_A, const std::vector<int> &col_in_A,
+    const std::vector<double> &B, const std::vector<int> &row_in_B, const std::vector<int> &col_in_B, int rows_A,
     int cols_A, int rows_B, int cols_B);
 void ConvertToCCS(const std::vector<std::vector<double>> &matrix, std::vector<double> &values,
                   std::vector<int> &row_indices, std::vector<int> &col_pointers);
 void FillData(std::shared_ptr<ppc::core::TaskData> &taskData, int rows_A, int cols_A, int rows_B, int cols_B,
-              std::vector<double> &A, std::vector<int> &row_ind_A, std::vector<int> &col_ind_A, std::vector<double> &B,
-              std::vector<int> &row_ind_B, std::vector<int> &col_ind_B, std::vector<std::vector<double>> &out);
+              std::vector<double> &A, std::vector<int> &row_in_A, std::vector<int> &col_in_A, std::vector<double> &B,
+              std::vector<int> &row_in_B, std::vector<int> &col_in_B, std::vector<std::vector<double>> &out);
 class TestTaskSequential : public ppc::core::Task {
  public:
   explicit TestTaskSequential(ppc::core::TaskDataPtr task_data) : Task(std::move(task_data)) {}
@@ -29,7 +29,7 @@ class TestTaskSequential : public ppc::core::Task {
  private:
   std::vector<std::vector<double>> ans;
   std::vector<double> A, B;
-  std::vector<int> row_ind_A, row_ind_B, col_ind_A, col_ind_B;
-  int rows_A, rows_B, cols_A, cols_B, size_A, size_B, row_ind_size_A, row_ind_size_B, col_ind_size_A, col_ind_size_B;
+  std::vector<int> row_in_A, row_in_B, col_in_A, col_in_B;
+  int rows_A, rows_B, cols_A, cols_B, size_A, size_B, row_in_size_A, row_in_size_B, col_in_size_A, col_in_size_B;
 };
 }  // namespace sedova_o_test_task_seq
--- tasks/seq/sedova_o_mult_matrices_ccs/src/ops_seq.cpp
+++ tasks/seq/sedova_o_mult_matrices_ccs/src/ops_seq.cpp
@@ -45,15 +45,15 @@ T sedova_o_test_task_seq::MultVectors(const std::vector<T>& vector_A, const std:
 }
 
 std::pair<std::vector<std::vector<double>>, std::vector<std::vector<double>>> sedova_o_test_task_seq::Convertirovanie(
-    const std::vector<double>& A, const std::vector<int>& row_ind_A, const std::vector<int>& col_ind_A,
-    const std::vector<double>& B, const std::vector<int>& row_ind_B, const std::vector<int>& col_ind_B, int rows_A,
+    const std::vector<double>& A, const std::vector<int>& row_in_A, const std::vector<int>& col_in_A,
+    const std::vector<double>& B, const std::vector<int>& row_in_B, const std::vector<int>& col_in_B, int rows_A,
     int cols_A, int rows_B, int cols_B) {
   std::vector<std::vector<double>> matrix_A(rows_A, std::vector<double>(cols_A, 0));
   for (int i = 0; i < rows_A; ++i) {
-    std::vector<double> v(col_ind_A.size() - 1, 0);
-    for (size_t j = 0; j < col_ind_A.size() - 1; ++j) {
-      for (int ind = col_ind_A[j]; ind < col_ind_A[j + 1]; ++ind) {
-        if (row_ind_A[ind] == i) {
+    std::vector<double> v(col_in_A.size() - 1, 0);
+    for (size_t j = 0; j < col_in_A.size() - 1; ++j) {
+      for (int ind = col_in_A[j]; ind < col_in_A[j + 1]; ++ind) {
+        if (row_in_A[ind] == i) {
           v[j] = A[ind];
         }
       }
@@ -61,16 +61,16 @@ std::pair<std::vector<std::vector<double>>, std::vector<std::vector<double>>> se
     matrix_A[i] = v;
   }
   std::vector<std::vector<double>> matrix_B(cols_B, std::vector<double>(rows_B, 0));
-  for (size_t i = 0; i < col_ind_B.size() - 1; ++i) {
+  for (size_t i = 0; i < col_in_B.size() - 1; ++i) {
     std::vector<double> v(rows_B, 0);
-    for (int ind = col_ind_B[i]; ind < col_ind_B[i + 1]; ++ind) {
-      v[row_ind_B[ind]] = B[ind];
+    for (int ind = col_in_B[i]; ind < col_in_B[i + 1]; ++ind) {
+      v[row_in_B[ind]] = B[ind];
     }
     matrix_B[i] = v;
   }
-  return std::pair<std::vector<std::vector<double>>, std::vector<std::vector<double>>>(matrixA, matrixB);
+  return std::pair<std::vector<std::vector<double>>, std::vector<std::vector<double>>>(matrix_A, matrix_B);
 }
-void sedova_o_test_task_seq::FillData(std::shared_ptr<ppc::core::task_data>& task_data, int rows_A, int cols_A,
+void sedova_o_test_task_seq::FillData(std::shared_ptr<ppc::core::TaskData>& task_data, int rows_A, int cols_A,
                                       int rows_B, int cols_B, std::vector<double>& A, std::vector<int>& row_in_A,
                                       std::vector<int>& col_in_A, std::vector<double>& B, std::vector<int>& row_in_B,
                                       std::vector<int>& col_in_B, std::vector<std::vector<double>>& out) {
@@ -104,41 +104,41 @@ bool sedova_o_test_task_seq::TestTaskSequential::PreProcessingImpl() {
   cols_A = task_data->inputs_count[1];
   size_A = task_data->inputs_count[2];
 
-  row_insize_A = task_data->inputs_count[3];
-  col_insize_A = task_data->inputs_count[4];
+  row_in_size_A = task_data->inputs_count[3];
+  col_in_size_A = task_data->inputs_count[4];
   rows_B = task_data->inputs_count[5];
   cols_B = task_data->inputs_count[6];
   size_B = task_data->inputs_count[7];
-  row_insize_B = task_data->inputs_count[8];
-  col_insize_B = task_data->inputs_count[9];
+  row_in_size_B = task_data->inputs_count[8];
+  col_in_size_B = task_data->inputs_count[9];
   A.resize(size_A);
   for (int i = 0; i < size_A; ++i) {
     auto* A_ptr = reinterpret_cast<double*>(task_data->inputs[0]);
     A[i] = A_ptr[i];
   }
-  row_in_A.resize(row_insize_A);
-  for (int i = 0; i < row_insize_A; ++i) {
+  row_in_A.resize(row_in_size_A);
+  for (int i = 0; i < row_in_size_A; ++i) {
     int* row_in_A_ptr = reinterpret_cast<int*>(task_data->inputs[1]);
     row_in_A[i] = row_in_A_ptr[i];
   }
-  col_in_A.resize(col_insize_A);
-  for (int i = 0; i < col_insize_A; ++i) {
+  col_in_A.resize(col_in_size_A);
+  for (int i = 0; i < col_in_size_A; ++i) {
     int* col_in_A_ptr = reinterpret_cast<int*>(task_data->inputs[2]);
     col_in_A[i] = col_in_A_ptr[i];
   }
 
-  B.resize(B_size);
-  for (int i = 0; i < B_size; ++i) {
+  B.resize(size_B);
+  for (int i = 0; i < size_B; ++i) {
     auto* B_ptr = reinterpret_cast<double*>(task_data->inputs[3]);
     B[i] = B_ptr[i];
   }
-  row_in_B.resize(row_in_B_size);
-  for (int i = 0; i < row_in_B_size; ++i) {
+  row_in_B.resize(row_in_size_B);
+  for (int i = 0; i < row_in_size_B; ++i) {
     int* row_in_B_ptr = reinterpret_cast<int*>(task_data->inputs[4]);
     row_in_B[i] = row_in_B_ptr[i];
   }
-  col_in_B.resize(col_in_B_size);
-  for (int i = 0; i < col_in_B_size; ++i) {
+  col_in_B.resize(col_in_size_B);
+  for (int i = 0; i < col_in_size_B; ++i) {
     int* col_in_B_ptr = reinterpret_cast<int*>(task_data->inputs[5]);
     col_in_B[i] = col_in_B_ptr[i];
   }
