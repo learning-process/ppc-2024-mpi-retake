fc3be762b4e8d68997b2eeb6b0743cd39d169de3
SingularityNN <mihamakhov@gmail.com>
2025-03-10 12:01:48 +0300

    func_tests fix 2

--- tasks/mpi/makhov_m_monte_carlo_method/func_tests/main.cpp
+++ tasks/mpi/makhov_m_monte_carlo_method/func_tests/main.cpp
@@ -19,7 +19,7 @@ TEST(makhov_m_monte_carlo_method, func_is_x_pow2) {
   std::string f = "x*x";
   int num_samples = 1000000;
   std::vector<std::pair<double, double>> limits = {{0.0, 1.0}};
-  double *answer_ptr = nullptr;
+  double answer_;
   double reference = 0.33;
 
   // Create TaskData
@@ -31,7 +31,7 @@ TEST(makhov_m_monte_carlo_method, func_is_x_pow2) {
     task_data_par->inputs_count.emplace_back(1);
     task_data_par->inputs_count.emplace_back(1);
     task_data_par->inputs_count.emplace_back(1);  // Integral dimension info
-    task_data_par->outputs.emplace_back(reinterpret_cast<uint8_t *>(answer_ptr));
+    task_data_par->outputs.emplace_back(reinterpret_cast<uint8_t *>(&answer_));
     task_data_par->outputs_count.emplace_back(1);
   }
 
@@ -56,7 +56,7 @@ TEST(makhov_m_monte_carlo_method, func_is_x_plus_y) {
   std::string f = "x+y";
   int num_samples = 1000000;
   std::vector<std::pair<double, double>> limits = {{0.0, 1.0}, {0.0, 1.0}};
-  double *answer_ptr = nullptr;
+  double answer_;
   double reference = 1.0;
 
   // Create TaskData
@@ -68,7 +68,7 @@ TEST(makhov_m_monte_carlo_method, func_is_x_plus_y) {
     task_data_par->inputs_count.emplace_back(1);
     task_data_par->inputs_count.emplace_back(1);
     task_data_par->inputs_count.emplace_back(2);  // Integral dimension info
-    task_data_par->outputs.emplace_back(reinterpret_cast<uint8_t *>(answer_ptr));
+    task_data_par->outputs.emplace_back(reinterpret_cast<uint8_t *>(&answer_));
     task_data_par->outputs_count.emplace_back(1);
   }
 
@@ -93,7 +93,7 @@ TEST(makhov_m_monte_carlo_method, func_is_xx_plus_yy) {
   std::string f = "x*x+y*y";
   int num_samples = 1000000;
   std::vector<std::pair<double, double>> limits = {{0.0, 1.0}, {0.0, 1.0}};
-  double *answer_ptr = nullptr;
+  double answer_;
   double reference = 0.67;
 
   // Create TaskData
@@ -105,7 +105,7 @@ TEST(makhov_m_monte_carlo_method, func_is_xx_plus_yy) {
     task_data_par->inputs_count.emplace_back(1);
     task_data_par->inputs_count.emplace_back(1);
     task_data_par->inputs_count.emplace_back(2);  // Integral dimension info
-    task_data_par->outputs.emplace_back(reinterpret_cast<uint8_t *>(answer_ptr));
+    task_data_par->outputs.emplace_back(reinterpret_cast<uint8_t *>(&answer_));
     task_data_par->outputs_count.emplace_back(1);
   }
 
@@ -130,7 +130,7 @@ TEST(makhov_m_monte_carlo_method, func_is_xx_plus_yy_plus_zz) {
   std::string f = "x*x+y*y+z*z";
   int num_samples = 1000000;
   std::vector<std::pair<double, double>> limits = {{0.0, 1.0}, {0.0, 1.0}, {0.0, 1.0}};
-  double *answer_ptr = nullptr;
+  double answer_;
   double reference = 1.0;
 
   // Create TaskData
@@ -142,7 +142,7 @@ TEST(makhov_m_monte_carlo_method, func_is_xx_plus_yy_plus_zz) {
     task_data_par->inputs_count.emplace_back(1);
     task_data_par->inputs_count.emplace_back(1);
     task_data_par->inputs_count.emplace_back(3);  // Integral dimension info
-    task_data_par->outputs.emplace_back(reinterpret_cast<uint8_t *>(answer_ptr));
+    task_data_par->outputs.emplace_back(reinterpret_cast<uint8_t *>(&answer_));
     task_data_par->outputs_count.emplace_back(1);
   }
 
